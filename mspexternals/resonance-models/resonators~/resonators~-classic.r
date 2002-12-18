f834fd5a5038951ce9a62fb8f726670a2c5d0811
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2002-12-18 23:58:05 +0000

    Added .r files so externals would have 'vers' resources.

---
+++ mspexternals/resonance-models/resonators~/resonators~-classic.r
@@ -0,0 +1,9 @@
+     resource 'vers' (1) {
+          0x01,               // Major release: digit before decimal
+          0x06,               // Minor release: digits after decimal
+          beta,               // dev/alpha/beta/release
+          0x00,               // Build # (for pre-release)
+          0,                  // Region code (US)
+          "1.6 (Classic)",        // Major/minor release as short string
+          "1.6beta, © 2002 CNMAT (Classic Version)"
+          };
