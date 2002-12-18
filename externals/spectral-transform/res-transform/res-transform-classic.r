98bd7ff335026e25d269a65a7572bcdd95b198bd
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2002-12-18 23:58:05 +0000

    Added .r files so externals would have 'vers' resources.

---
+++ externals/spectral-transform/res-transform/res-transform-classic.r
@@ -0,0 +1,9 @@
+     resource 'vers' (1) {
+          0x01,               // Major release: digit before decimal
+          0x03,               // Minor release: digits after decimal
+          release,               // dev/alpha/beta/release
+          0x00,               // Build # (for pre-release)
+          0,                  // Region code (US)
+          "1.3 (Classic)",        // Major/minor release as short string
+          "1.3, © 2002 CNMAT (Classic Version)"
+          };
---
+++ mspexternals/filters/peqbank~/peqbank~-osx.r
@@ -0,0 +1,9 @@
+     resource 'vers' (1) {
+          0x01,               // Major release: digit before decimal
+          0x08,               // Minor release: digits after decimal
+          release,               // dev/alpha/beta/release
+          0x00,               // Build # (for pre-release)
+          0,                  // Region code (US)
+          "1.8 (OSX)",        // Major/minor release as short string
+          "1.8, © 2002 CNMAT (OSX Version)"
+          };
