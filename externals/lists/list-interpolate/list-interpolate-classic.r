8cc62f861072d8e2b98dc2bb0224feb604dde529
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2002-12-20 04:51:00 +0000

    Built new mcp for classic for the first time with .r file.

--- externals/lists/list-interpolate/list-interpolate-classic.r
+++ externals/lists/list-interpolate/list-interpolate-classic.r
@@ -1,6 +1,6 @@
      resource 'vers' (1) {
           0x01,               // Major release: digit before decimal
-          0x03,               // Minor release: digits after decimal
+          0x05,               // Minor release: digits after decimal
           release,               // dev/alpha/beta/release
           0x00,               // Build # (for pre-release)
           0,                  // Region code (US)
