7c16dcc274d7e3cdb8af61f1749e25dd75afb192
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2003-03-13 23:45:17 +0000

    Version 1.7 has bandwidth enhancement, better birth+death, "tellmeeverything", and minor bug fixes.

--- mspexternals/additive/sinusoids~/sinusoids~-osx.r
+++ mspexternals/additive/sinusoids~/sinusoids~-osx.r
@@ -1,9 +1,9 @@
      resource 'vers' (1) {
           0x01,               // Major release: digit before decimal
-          0x06,               // Minor release: digits after decimal
+          0x07,               // Minor release: digits after decimal
           release,               // dev/alpha/beta/release
           0x00,               // Build # (for pre-release)
           0,                  // Region code (US)
-          "1.6 (OSX)",        // Major/minor release as short string
-          "1.6, © 2002 CNMAT (OSX Version)"
+          "1.7 (OSX)",        // Major/minor release as short string
+          "1.7, © 2002 CNMAT (OSX Version)"
           };
