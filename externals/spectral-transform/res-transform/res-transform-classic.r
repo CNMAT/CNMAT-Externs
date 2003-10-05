5ad11d672481c49079cfb2e6674c486eb893193b
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2003-10-05 21:03:41 +0000

    *** empty log message ***

--- externals/spectral-transform/res-transform/res-transform-classic.r
+++ externals/spectral-transform/res-transform/res-transform-classic.r
@@ -1,9 +1,9 @@
      resource 'vers' (1) {
           0x01,               // Major release: digit before decimal
-          0x04,               // Minor release: digits after decimal
+          0x05,               // Minor release: digits after decimal
           release,               // dev/alpha/beta/release
           0x00,               // Build # (for pre-release)
           0,                  // Region code (US)
-          "1.4 (Classic)",        // Major/minor release as short string
-          "1.4, © 2002 CNMAT (Classic Version)"
+          "1.5 (Classic)",        // Major/minor release as short string
+          "1.5, © 2002,3 CNMAT (Classic Version)"
           };
