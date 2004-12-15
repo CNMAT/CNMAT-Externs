e164ce17dc5a23aefbbfd3c518d1e30a8d365899
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2004-12-15 03:24:02 +0000

    The code changed so much that the old OS9 version became a separate branch, so the classic.r file doesn't belong here any more.

--- externals/networking/otudp/otudp-classic.r
+++
@@ -1,9 +0,0 @@
-     resource 'vers' (1) {
-          0x02,               // Major release: digit before decimal
-          0x03,               // Minor release: digits after decimal
-          release,               // dev/alpha/beta/release
-          0x00,               // Build # (for pre-release)
-          0,                  // Region code (US)
-          "2.3 (Classic)",        // Major/minor release as short string
-          "2.3, © 2002 CNMAT (Classic Version)"
-          };
