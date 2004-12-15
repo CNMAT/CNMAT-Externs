9ace94c2da5ce46257dacd472d47fc9b0b21692f
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2004-12-15 03:47:16 +0000

    Copied over .r files from my working directory.  I guess I checked in just Brian Vogel's source code to CVS initially, then added the versioning stuff to my working copy.

---
+++ mspexternals/analysis/pitcher~/pitcher~-classic.r
@@ -0,0 +1,9 @@
+     resource 'vers' (1) {
+          0x00,               // Major release: digit before decimal
+          0x06,               // Minor release: digits after decimal
+          release,               // dev/alpha/beta/release
+          0x00,               // Build # (for pre-release)
+          0,                  // Region code (US)
+          "0.6 (Classic)",        // Major/minor release as short string
+          "0.6, © 2002 CNMAT (Classic Version)"
+          };
