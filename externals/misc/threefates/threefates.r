4750307507766eb0d180790d158b0ea71e541140
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2005-01-01 01:38:10 +0000

    Made hex version numbers (which don't seem to do anything) be 0xff just to emphasize their lack of information content.

--- externals/misc/threefates/threefates.r
+++ externals/misc/threefates/threefates.r
@@ -1,8 +1,8 @@
 #include "VERSION.h"
 
      resource 'vers' (1) {
-          0x00,               // Major release: digit before decimal
-          0x88,               // Minor release: digits after decimal
+          0xff,               // Major release: digit before decimal
+          0xff,               // Minor release: digits after decimal
           release,               // dev/alpha/beta/release
           0x00,               // Build # (for pre-release)
           0,                  // Region code (US)
