4b766c7498b7f43144905b4724e7543198ed5a0d
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2005-03-21 23:08:42 +0000

    Andy's first version, emailed to Matt.

---
+++ commentkeys.pl
@@ -0,0 +1,27 @@
+##
+# commentkeys.pl
+#
+# by Andy W. Schmeder <andy@a2hd.com> 2005
+#
+# run as "perl commentkeys.pl < test.c > test.info.c" 
+#     or "echo test.c | perl commentkeys.pl > test.info.txt"
+#
+
+my $source = '';
+
+while(<STDIN>) {
+  $source .= $_
+}
+
+while($source =~ m~/\*((\n|.)*?)\*/~g) {
+    my $block_comment = $1;
+    while($block_comment =~ m~\s*\@\@+\s*\n?((\n|.)*?)\n?\s*\@\@+~mg) {
+        my $info_block = $1;
+	while($info_block =~ m~\s*\*?\s*([A-Za-z0-9_\.-]*)\s*:\s*(.*)\s*~mg) {
+	    my ($key, $value) = ($1, $2);
+	    $value =~ s~\\~\\\\~g;
+	    $value =~ s~"~\\"~g;
+	    print qq~#define $key "$value"\n~; 
+	}
+    }
+}
