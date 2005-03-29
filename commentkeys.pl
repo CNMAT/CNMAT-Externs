437ccbe1e18508edcdd4920052f0f73fcca378f4
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2005-03-29 01:15:10 +0000

    New version from Andy handles a name/value entry for each (old) version of the C file, so version history can be automatic on the web, etc.

--- commentkeys.pl
+++ commentkeys.pl
@@ -7,21 +7,74 @@
 #     or "echo test.c | perl commentkeys.pl > test.info.txt"
 #
 
+my $header_mode = 0;
+if($ARGV[0] eq "-header") {
+    $header_mode = 1;
+}
+
+my @versions = ();
 my $source = '';
 
 while(<STDIN>) {
   $source .= $_
 }
 
+# natural-order sort with '.' separator and '-' tags
+sub version_sort {
+    my @av = split /\./, $a;
+    my @bv = split /\./, $b;
+
+    my $i = 0;
+    while ($i < @av && $i < @bv) {
+	my ($anum, $atag) = split /-/, $av[$i];
+	my ($bnum, $btag) = split /-/, $bv[$i];
+
+	if(int($anum) < int($bnum)) {
+	    return -1;
+	} elsif(int($anum) > int($bnum)) {
+	    return 1;
+	} else {
+	    if($atag && ! $btag) {  # no tag sorts higher
+		return -1;
+	    }
+	    elsif($btag && ! $atag) {
+		return 1;
+	    } elsif($atag && $btag) {
+		return $atag < $btag ? -1 : 1
+	    }
+	}
+	$i++;
+    }
+
+    return 0;
+}
+
 while($source =~ m~/\*((\n|.)*?)\*/~g) {
     my $block_comment = $1;
     while($block_comment =~ m~\s*\@\@+\s*\n?((\n|.)*?)\n?\s*\@\@+~mg) {
         my $info_block = $1;
-	while($info_block =~ m~\s*\*?\s*([A-Za-z0-9_\.-]*)\s*?:[ ]*([^\n]*)\s*~mg) {
+	while($info_block =~ m~\s*\*?\s*([A-Za-z0-9_\. -]*)\s*?:[ ]*([^\n]*)\s*~mg) {
 	    my ($key, $value) = ($1, $2);
 	    $value =~ s~\\~\\\\~g;
 	    $value =~ s~"~\\"~g;
-	    print qq~#define $key "$value"\n~; 
+	    if($key =~ /^VERSION(\s|-)*(.*)$/i) {
+		push @versions, $2;
+	    }
+	    if($header_mode) {
+		if(! ($key =~ /^VERSION/i)) {
+		    print qq~#define $key "$value"\n~; 
+		}
+	    } else {
+		print qq~$key: "$value"\n~; 
+	    }
 	}
     }
+}
+
+@versions = reverse sort version_sort @versions;
+
+if($header_mode) {
+    print qq~#define VERSION "$versions[0]"\n~;
+} else {
+    print qq~VERSION: "$versions[0]"\n~;
 }
