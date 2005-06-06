56ac03dec936fea1066bfe9f1f16c5e92dad889f
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2005-06-06 21:48:21 +0000

    Made new .mcp with current XML templates, with a single .r file for both OS9 and OSX.

--- externals/lists/list-interpolate/list-interpolate-osx.r
+++
@@ -1,9 +0,0 @@
-     resource 'vers' (1) {
-          0x01,               // Major release: digit before decimal
-          0x05,               // Minor release: digits after decimal
-          release,               // dev/alpha/beta/release
-          0x00,               // Build # (for pre-release)
-          0,                  // Region code (US)
-          "1.5 (OSX)",        // Major/minor release as short string
-          "1.5, © 2002 CNMAT (OSX Version)"
-          };
