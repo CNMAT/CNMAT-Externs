9b4c2d115a46466e0ebc48e5078da20739251cde
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2005-12-02 01:04:30 +0000

    VERSION 1.11: Allows multi-level prefixes (e.g., "/foo/bar").

--- externals/misc/OSC-route/OSC-route.c
+++ externals/misc/OSC-route/OSC-route.c
@@ -43,6 +43,7 @@ VERSION 1.10: Assitance now says "for prefix /%s (subaddress + arguments)"
 VERSION 1.10.1: New versioning system
 VERSION 1.10.2: Version info in this .c file
 VERSION 1.10.3: Just a test of incrementing the version number
+VERSION 1.11: Allows multi-level prefixes (e.g., "/foo/bar")
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
  */
@@ -72,7 +73,10 @@ typedef struct OSCroute
 	Object o_ob;				// required header
 	int o_num;					// Number of address prefixes we store
 	int o_complainmode;			// Do we print a message if no match?
-	char *o_prefixes[MAX_NUM];
+	char *o_prefixes[MAX_NUM];	// Prefixes this object matches, with multiple levels as successive 
+								// null-terminated strings
+	int prefix_levels[MAX_NUM]; // Number of levels (i.e., of strings) for each prefix
+	int prefix_sizes[MAX_NUM];	// # bytes in each o_prefixes[i], for freebytes
 	void *o_outlets[MAX_NUM];
 	void *o_otheroutlet;		// "none of the above" outlet
 } OSCroute;
@@ -87,8 +91,11 @@ void *OSCroute_class;
 
 /* prototypes  */
 
+void OSCroute_free(OSCroute *x);
+int RememberPrefix (OSCroute *x, char *prefixWithLeadingSlash);
 Boolean MyPatternMatch (const char *pattern, const char *test);
 void OSCroute_doanything(OSCroute *x, Symbol *s, short argc, Atom *argv);
+static void OutputOSCArguments(OSCroute *x, int i, short argc, Atom *argv);
 void OSCroute_anything(OSCroute *x, Symbol *s, short argc, Atom *argv);
 void OSCroute_list(OSCroute *x, Symbol *s, short argc, Atom *argv);
 void *OSCroute_new(Symbol *s, short argc, Atom *argv);
@@ -97,15 +104,16 @@ void OSCroute_assist (OSCroute *x, void *box, long msg, long arg, char *dstStrin
 void OSCroute_allmessages(OSCroute *x, Symbol *s, short argc, Atom *argv);
 void OSCroute_set(OSCroute *x, long outlet, Symbol *s);
 static char *NextSlashOrNull(char *p);
-static void StrCopyUntilSlash(char *target, const char *source);
+static int StrCopyUntilSlash(char *target, const char *source);
+static int MyStrCopy(char *target, const char *source);
+static int MyStrLen(const char *s);
 
 
 /* initialization routine */
 
-void main(fptr *f)
-{
-	/* tell Max about your class. The cast to short is important for 68K */
-	setup((t_messlist **)&OSCroute_class, (method)OSCroute_new,0L, (short)sizeof(OSCroute), 0L, A_GIMME, 0);
+void main(fptr *f) {
+	setup((t_messlist **)&OSCroute_class, (method)OSCroute_new, (method)OSCroute_free, 
+		  (short)sizeof(OSCroute), 0L, A_GIMME, 0);
 	/* bind your methods to symbols */
 	addmess((method)OSCroute_anything, "anything", A_GIMME, 0);
 	addmess((method)OSCroute_list, "list", A_GIMME, 0);
@@ -128,26 +136,55 @@ void main(fptr *f)
 
 /* instance creation routine */
 
+
+int RememberPrefix (OSCroute *x, char *prefixWithLeadingSlash) {
+	char *s;
+	
+	if (x->o_num >= MAX_NUM) {
+		error("OSC-route: too many outlets requested. (max %ld)", MAX_NUM);
+		return 0;
+	}
+
+	x->prefix_sizes[x->o_num] = MyStrLen(prefixWithLeadingSlash);
+	x->o_prefixes[x->o_num] = getbytes(x->prefix_sizes[x->o_num]);
+	if (x->o_prefixes[x->o_num] == 0) {
+		error("Out of memory saving string %s", prefixWithLeadingSlash);
+		return 0;
+	}
+	
+	// Skip the opening slash
+	MyStrCopy(x->o_prefixes[x->o_num], prefixWithLeadingSlash + 1);
+	x->prefix_levels[x->o_num] = 1;
+
+	// Convert all other slashes to nulls so as to
+	// store each "part" (between slashes) as consecutive strings.
+	for (s = x->o_prefixes[x->o_num]; *s != '\0'; ++s) {
+		if (*s == '/') {
+			*s = '\0';
+			++(x->prefix_levels[x->o_num]);
+		}
+	}
+				
+	++(x->o_num);
+	return 1;
+}
+
 void *OSCroute_new(Symbol *s, short argc, Atom *argv)
 {
 	OSCroute *x;
-	int i, n;
+	int i, j, n;
+	char *str;
 	
 	x = newobject(OSCroute_class);		// get memory for a new object & initialize
 
 	x->o_complainmode = 0;
 	x->o_num = 0;
 	for (i = 0; i < argc; ++i) {
-		if (x->o_num >= MAX_NUM) {
-			post("¥ OSC-route: too many outlets requested. (max %ld)", MAX_NUM);
-			return 0;
-		}
 
 		if (argv[i].a_type == A_SYM) {
 			if (argv[i].a_w.w_sym->s_name[0] == '/') {
-				/* Now that's a nice prefix */
-				x->o_prefixes[x->o_num] = argv[i].a_w.w_sym->s_name + 1;
-				++(x->o_num);
+				/* Now that's a nice prefix. */
+				if (RememberPrefix(x, argv[i].a_w.w_sym->s_name) == 0) return 0;
 			} else if (argv[i].a_w.w_sym->s_name[0] == '#' &&
 					   argv[i].a_w.w_sym->s_name[1] >= '1' &&
 					   argv[i].a_w.w_sym->s_name[1] <= '9') {
@@ -166,19 +203,12 @@ void *OSCroute_new(Symbol *s, short argc, Atom *argv)
 				}
 				
 				if (argv[i+1].a_type == A_SYM) {
-					x->o_prefixes[x->o_num] = argv[i+1].a_w.w_sym->s_name;
-					++(x->o_num);
+					if (RememberPrefix(x, argv[i].a_w.w_sym->s_name) == 0) return 0;
 				} else if (argv[i+1].a_type == A_LONG) {
 					// Convert to a numeral.  Max ints are -2147483648 to 2147483647
-					char *string = getbytes(12);
-					// I can't be bothered to plug this 12 byte memory leak
-					if (string == 0) {
-						post("¥ OSC-route: out of memory!");
-						return 0;
-					}
+					char string[12];
 					sprintf(string, "%d", argv[i+1].a_w.w_long);
-					x->o_prefixes[x->o_num] = string;
-					++(x->o_num);
+					if (RememberPrefix(x, string) == 0) return 0;
 				} else {
 					post("¥ OSC-route: unrecognized argument type after \"slash\".");
 					return 0;
@@ -199,6 +229,14 @@ void *OSCroute_new(Symbol *s, short argc, Atom *argv)
 		}
 	}
 	
+	/*
+	for (i = 0; i<x->o_num; ++i) {
+		post("prefix[%d] has %d levels:", i, x->prefix_levels[i]);
+		for (j = 0, str=x->o_prefixes[i]; j<x->prefix_levels[i]; ++j, str=NextSlashOrNull(str)+1) {
+			post("   %d: %s", j, str);
+		}
+	}
+	*/
 	
 	/* Have to create the outlets in reverse order */
 	
@@ -211,6 +249,14 @@ void *OSCroute_new(Symbol *s, short argc, Atom *argv)
 	return (x);
 }
 
+
+
+void OSCroute_free(OSCroute *x) {
+	int i;
+	for (i = 0; i<x->o_num; ++i) {
+		freebytes(x->o_prefixes[i], x->prefix_sizes[i]);
+	}
+}
 
 void OSCroute_version (OSCroute *x) {
 	post(NAME " Version " VERSION
@@ -230,7 +276,14 @@ void OSCroute_assist (OSCroute *x, void *box, long msg, long arg, char *dstStrin
 		} else if (arg < 0 || arg > x->o_num) {
 			post("¥ OSCroute_assist: No outlet corresponds to arg %ld!", arg);
 		} else {
-			sprintf(dstString, "for prefix /%s (subaddress + arguments)", x->o_prefixes[arg]);
+			char *str;
+			int j;
+			int i = MyStrCopy(dstString, "for prefix ");
+			for (j = 0, str=x->o_prefixes[arg]; j<x->prefix_levels[arg]; ++j, str=NextSlashOrNull(str)+1) {
+				i += MyStrCopy(dstString+i, "/");
+				i += MyStrCopy(dstString+i, str);
+			}
+			MyStrCopy(dstString+i, " (subaddress + arguments)");
 		}
 	} else {
 		post("¥ OSCroute_assist: unrecognized message %ld", msg);
@@ -271,100 +324,131 @@ void OSCroute_anything(OSCroute *x, Symbol *s, short argc, Atom *argv) {
 	OSCroute_doanything(x, s, argc, argv);
 }
 	
+
 Boolean MyPatternMatch (const char *pattern, const char *test) {
-	/* This allows the special case of "OSC-route /*" to be an outlet
-	   that matches anything; i.e., it always outputs the input with the first
-	   level of the address stripped off. */
 	if (test[0] == '*' && test[1] == '\0') {
+		/* This allows the special case of "OSC-route /*" to be an outlet
+		   that matches anything; i.e., it always outputs the input with the first
+		   level of the address stripped off. */
 		return 1;
 	} else {
 		return PatternMatch(pattern, test);
 	}
 }
 
+
+#define MAX_PREFIX_LEVELS 20  // e.g., "/a/b/c/d/e/f/ ... /t"
+#define MAX_PREFIX_PART_LENGTH 50
 void OSCroute_doanything(OSCroute *x, Symbol *s, short argc, Atom *argv) {
-	char *pattern, *nextSlash;
-	int i;
+	char *pattern, *nextSlash, *str;
+	int i,j,k;
 	int matchedAnything;
+	char patternParts[MAX_PREFIX_LEVELS][MAX_PREFIX_PART_LENGTH];
+	int numPatternParts;
+	
 	// post("*** OSCroute_anything(s %s, argc %ld)", s->s_name, (long) argc);
 	
+	
+	// Decompose incoming pattern into its parts
 	pattern = s->s_name;
 	if (pattern[0] != '/') {
 		post("¥ OSC-route: invalid message pattern %s does not begin with /", s->s_name);
 		return;
 	}
+	pattern++; // Skip opening slash
+
+	numPatternParts = 0;
+	while (numPatternParts < MAX_PREFIX_LEVELS) {
+		pattern += StrCopyUntilSlash(patternParts[numPatternParts], pattern);
+		++numPatternParts;
+		if (*pattern == '\0') break;
+		pattern++; // skip this slash
+	}
 	
+	if (numPatternParts == MAX_PREFIX_LEVELS) {
+		error("OSC-route: exceeded MAX_PREFIX_LEVELS");
+		return;
+	}
+	
+	/*
+	post("Pattern has %d parts:", numPatternParts);
+	for (i=0; i<numPatternParts; ++i) {
+		post("  %s", patternParts[i]);
+	}
+	*/
+	
+	/* Go through all our prefixes looking for matches */
 	matchedAnything = 0;
 	
-	nextSlash = NextSlashOrNull(pattern+1);
-	if (*nextSlash == '\0') {
-		/* last level of the address, so we'll output the argument list */
-		
-
-#ifdef NULL_IS_DIFFERENT_FROM_BANG
-		if (argc==0) {
-			post("¥ OSC-route: why are you matching one level pattern %s with no args?",
-				 pattern);
-			return;
-		}
-#endif
-		
-		for (i = 0; i < x->o_num; ++i) {
-			if (MyPatternMatch(pattern+1, x->o_prefixes[i])) {
-				++matchedAnything;
-				
-				// I hate stupid Max lists with a special first element
-				if (argc == 0) {
-					outlet_bang(x->o_outlets[i]);
-				} else if (argv[0].a_type == A_SYM) {
-					// Promote the symbol that was argv[0] to the special symbol
-					outlet_anything(x->o_outlets[i], argv[0].a_w.w_sym, argc-1, argv+1);
-				} else if (argc > 1) {
-					// Multiple arguments starting with a number, so naturally we have
-					// to use a special function to output this "list", since it's what
-					// Max originally meant by "list".
-					outlet_list(x->o_outlets[i], 0L, argc, argv);
-				} else {
-					// There was only one argument, and it was a number, so we output it
-					// not as a list
-					if (argv[0].a_type == A_LONG) {
-						outlet_int(x->o_outlets[i], argv[0].a_w.w_long);
-					} else if (argv[0].a_type == A_FLOAT) {
-						outlet_float(x->o_outlets[i], argv[0].a_w.w_float);
-					} else {
-						post("¥ OSC-route: unrecognized atom type!");
-					}
-				}
+	for (i = 0; i < x->o_num; ++i) {
+		str = x->o_prefixes[i];
+		for (j = 0; j < x->prefix_levels[i] && j < numPatternParts; ++j) {
+			if (MyPatternMatch(patternParts[j], str)) {
+				// post("matched %s against %s", patternParts[j], str);
+				str = NextSlashOrNull(str) + 1;
+			} else {
+				// post("didn't match %s against %s", patternParts[j], str);
+				break;
 			}
 		}
-	} else {
-		/* There's more address after this part, so our output list will begin with
-		   the next slash.  */
-		Symbol *restOfPattern = 0; /* avoid the gensym unless we have to output */
-		char patternBegin[1000];
+		// post("i %d j %d numPatternParts %d previxLevels", i, j, numPatternParts, x->prefix_levels[i]);
 		
-		
-		/* Get the first level of the incoming pattern to match against all our prefixes */
-		StrCopyUntilSlash(patternBegin, pattern+1);
-
-		for (i = 0; i < x->o_num; ++i) {
-			if (MyPatternMatch(patternBegin, x->o_prefixes[i])) {
-				++matchedAnything;
-				if (restOfPattern == 0) {
-					restOfPattern = gensym(nextSlash);
+		if (j == x->prefix_levels[i]) {
+			// We matched all levels of the prefix...
+			++matchedAnything;
+			if (j == numPatternParts) {
+				// ...against all parts of the pattern, so output the arguments
+				OutputOSCArguments(x, i, argc, argv);
+			} else {
+				// ...against the beginning of the pattern, so output the rest of the pattern plus arguments
+				char dstString[1000];
+				Symbol *restOfPattern;
+				k = 0;
+				for (/* j already first unmatched pattern part */; j<numPatternParts; ++j) {
+					k += MyStrCopy(dstString+k, "/");
+					k += MyStrCopy(dstString+k, patternParts[j]);
 				}
+				restOfPattern = gensym(dstString);
 				outlet_anything(x->o_outlets[i], restOfPattern, argc, argv);
 			}
 		}
-	}	
-	
-	
+	}
+		
 	if (!matchedAnything) {
 		if (x->o_complainmode) {
 			post("¥ OSC-route: pattern %s did not match any prefixes", pattern);
 		}
 
 		outlet_anything(x->o_otheroutlet, s, argc, argv);
+	}
+}
+
+
+static void OutputOSCArguments(OSCroute *x, int i, short argc, Atom *argv) {
+	// We've matched  the entire OSC address pattern, so output the OSC arguments
+	// as a Max list.
+	 
+	// I hate stupid Max lists with a special first element
+	if (argc == 0) {
+		outlet_bang(x->o_outlets[i]);
+	} else if (argv[0].a_type == A_SYM) {
+		// Promote the symbol that was argv[0] to the special symbol
+		outlet_anything(x->o_outlets[i], argv[0].a_w.w_sym, argc-1, argv+1);
+	} else if (argc > 1) {
+		// Multiple arguments starting with a number, so naturally we have
+		// to use a special function to output this "list", since it's what
+		// Max originally meant by "list".
+		outlet_list(x->o_outlets[i], 0L, argc, argv);
+	} else {
+		// There was only one argument, and it was a number, so we output it
+		// not as a list
+		if (argv[0].a_type == A_LONG) {
+			outlet_int(x->o_outlets[i], argv[0].a_w.w_long);
+		} else if (argv[0].a_type == A_FLOAT) {
+			outlet_float(x->o_outlets[i], argv[0].a_w.w_float);
+		} else {
+			post("¥ OSC-route: unrecognized atom type!");
+		}
 	}
 }
 
@@ -375,13 +459,16 @@ static char *NextSlashOrNull(char *p) {
     return p;
 }
 
-static void StrCopyUntilSlash(char *target, const char *source) {
+static int StrCopyUntilSlash(char *target, const char *source) {
+	int i = 0;
 	while (*source != '/' && *source != '\0') {
 		*target = *source;
 		++target;
 		++source;
+		++i;
 	}
 	*target = 0;
+	return i;
 }
 
 static int MyStrCopy(char *target, const char *source) {
@@ -393,6 +480,15 @@ static int MyStrCopy(char *target, const char *source) {
 		++i;
 	}
 	*target = 0;
+	return i;
+}
+
+static int MyStrLen(const char *s) {
+	int i = 0;
+	while (*s != '\0') {
+		++s;
+		++i;
+	}
 	return i;
 }
 
