0a477c1320fba93163f66841f77d04e0e6195bb9
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2006-01-23 21:17:10 +0000

    Added copyright notice to all C code; inserted meaningless variable names into prototypes.h to remove hundreds of compiler errors.  Still won't compile.

--- externals/machine-learning/MLP/prototypes.h
+++ externals/machine-learning/MLP/prototypes.h
@@ -1,75 +1,104 @@
+/* Copyright (c) 1990-2006.  The Regents of the University of California (Regents).
+All Rights Reserved.
+
+Permission to use, copy, modify, and distribute this software and its
+documentation for educational, research, and not-for-profit purposes, without
+fee and without a signed licensing agreement, is hereby granted, provided that
+the above copyright notice, this paragraph and the following two paragraphs
+appear in all copies, modifications, and distributions.  Contact The Office of
+Technology Licensing, UC Berkeley, 2150 Shattuck Avenue, Suite 510, Berkeley,
+CA 94720-1620, (510) 643-7201, for commercial licensing opportunities.
+
+Written by Mike Lee, The Center for New Music and Audio Technologies,
+University of California, Berkeley.
+
+     IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+     SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+     ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+     REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+     REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+     LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+     FOR A PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING
+     DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS".
+     REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+     ENHANCEMENTS, OR MODIFICATIONS.
+*/
+
 /*---------------------------------------------------------------------------*
 
 		prototypes.h
 		
 *----------------------------------------------------------------------------*/
+/* Matt Wright put in dummy variable names for everything on 060123 to make it compile again. */
+
 
 #ifdef never
 int		atoi(char *);
 #endif
-void	*bpAdjustSlopes(NNetPtr,double *);
-void	*bpHiddenDeltas(NNetPtr);
-void	*bpOutputDeltas(NNetPtr);
-void	*bpAdjustWeights(NNetPtr);
-void	*bpInputErrors(NNetPtr);
-void	*bpInError(NNetPtr,double *,double *);
+void	*bpAdjustSlopes(NNetPtr a,double *b);
+void	*bpHiddenDeltas(NNetPtr a);
+void	*bpOutputDeltas(NNetPtr a);
+void	*bpAdjustWeights(NNetPtr a);
+void	*bpInputErrors(NNetPtr a);
+void	*bpInError(NNetPtr a,double *b,double *c);
 
-extern	void	*bpLearn(NNetPtr,double*,double*,double*,double*,double*,double,Boolean);
+extern	void	*bpLearn(NNetPtr a,double* b,double* v,double* d,double* e,double* f,double g,Boolean h);
 
-double	bpNoPropLearn(NNetPtr,double *);
-void	*buildFunctionTable(void);
+double	bpNoPropLearn(NNetPtr a,double * b);
+void	*buildFunctionTable(void );
 
-double	calculateError(NNetPtr,double *);
-NNetPtr	createNet(int,int,int,int,double,double,double,double,fType,fType);
+double	calculateError(NNetPtr a,double * b);
+NNetPtr	createNet(int a,int v,int c,int d,double e,double f,double g,double h,fType  i,fType j);
 
-int		createIOLinks(NNetPtr);
-linkPtr	createLink(unitPtr,linkPtr,linkPtr,int,linkPtr,int,char *,double,double);
-int		createProcessingUnits(NNetPtr,fType,fType);
-void	*createUnit(unitPtr,int,char *, int,
-					void (*)(unitPtr,double,double),
-					void (*)(unitPtr),double,void (*)(unitPtr),double);
+int		createIOLinks(NNetPtr a);
+linkPtr	createLink(unitPtr a,linkPtr b,linkPtr c,int d,linkPtr e,int f,char * g,double h,double i);
+int		createProcessingUnits(NNetPtr a,fType b,fType c);
+void	*createUnit(unitPtr a,int v,char * b, int c,
+					void (*d)(unitPtr,double,double),
+					void (*e)(unitPtr),double f,void (*g)(unitPtr),double h);
 
-void	*disposeGraphics(NeuralNetPtr);
-void	*drawNeuron(NeuralNetPtr,int);
-void	*drawNeurons(NeuralNetPtr);
-void	*DrawPict(NeuralNetPtr,int);
-void	*drawWeight(NNetPtr,int);
-void	*drawWeights(NeuralNetPtr);
-void	*eraseGraphics(NeuralNetPtr);
-void	*freeNetwork(NNetPtr);
+void	*disposeGraphics(NeuralNetPtr a);
+void	*drawNeuron(NeuralNetPtr a,int b);
+void	*drawNeurons(NeuralNetPtr a);
+void	*DrawPict(NeuralNetPtr a,int b);
+void	*drawWeight(NNetPtr r,int e);
+void	*drawWeights(NeuralNetPtr a);
+void	*eraseGraphics(NeuralNetPtr a);
+void	*freeNetwork(NNetPtr a);
 #ifdef MAC
-void	*GetDlgTextItem(DialogPtr,int,char *);
-Point	GetPictSize(int);
-Rect	GetPictRect(int);
+void	*GetDlgTextItem(DialogPtr a,int b,char * c);
+Point	GetPictSize(int a);
+Rect	GetPictRect(int a);
 #endif
-double	getSlopeFactorHid(NNetPtr,long);
-double	getSlopeFactorOut(NNetPtr,long);
-void	*getWeights(NNetPtr,double *);
+double	getSlopeFactorHid(NNetPtr a,long b);
+double	getSlopeFactorOut(NNetPtr a,long b);
+void	*getWeights(NNetPtr a,double * r);
 
 #ifndef MAXObject
 char	get_command(void);
 long	get_iterations(void);
-short	get_numb(char*);
-float	get_num(char*);
-short	get_str(char*);
-short	get_vector(int,double*);
-void 	*nn_info(short*,short*,short*,short*,double*,double*,double*,double*,short*,short*);
+short	get_numb(char* a);
+float	get_num(char* a);
+short	get_str(char* a);
+short	get_vector(int a ,double* b);
+void 	*nn_info(short* a,short* b,short* c,short* d,double* e,double* f,double* g,double* h,short* i,short* j);
 #else
-void	*nn_info(NeuralNetPtr,void *);
-void	*info(NeuralNetPtr,void *);
+void	*nn_info(NeuralNetPtr a,void * b);
+void	*info(NeuralNetPtr a,void * b);
 #endif
 
 #ifdef MAC
-void	*HiliteDefaultButton(DialogPtr);
+void	*HiliteDefaultButton(DialogPtr a);
 #endif
 
-void	*initialiseInputUnits(NNetPtr,double *,double *);
-void	*initialiseNeurons(NeuralNetPtr);
+void	*initialiseInputUnits(NNetPtr a,double * b,double * v);
+void	*initialiseNeurons(NeuralNetPtr a);
 
-NeuralNetPtr	newNeuralNet(int,int,int,int,double,double,double,double,fType,fType);
+NeuralNetPtr	newNeuralNet(int a,int b,int c,int d,double e,double f,double g,double h,fType i,fType j);
 
 #ifndef MAXObject
-double	nn_auto_learn(NeuralNetPtr,long);
+double	nn_auto_learn(NeuralNetPtr ,long);
 void	*nn_recognize(NeuralNetPtr,double*,double*);
 #else
 void	*nn_auto_learn(NeuralNetPtr,long);
@@ -83,98 +112,98 @@ void	*nn_outlet_list(struct outlet *,char *,int,Atom *);
 void	*nn_recognize(NeuralNetPtr,Symbol *,int,Atom *);
 #endif
 
-void	nn_bang(NeuralNetPtr);
+void	nn_bang(NeuralNetPtr a);
 #ifdef MAC
-void	*nn_click(NeuralNetPtr,Point,int);
+void	*nn_click(NeuralNetPtr a,Point b,int c);
 #endif
-void	*nn_debug(NeuralNetPtr);
+void	*nn_debug(NeuralNetPtr a);
 #ifdef MAXObject
-void	*nn_doget_pattern(NeuralNetPtr,patternSetPtr,Symbol*);
-void	*nn_doread(NeuralNetPtr,Symbol*);
-void	*nn_dowrite(NeuralNetPtr,Symbol*);
+void	*nn_doget_pattern(NeuralNetPtr a,patternSetPtr r,Symbol* c);
+void	*nn_doread(NeuralNetPtr a,Symbol* b);
+void	*nn_dowrite(NeuralNetPtr a,Symbol* b);
 #else
-void	*nn_doget_pattern(NeuralNetPtr,patternSetPtr,char*);
-void	*nn_doread(NeuralNetPtr,char*);
-void	*nn_dowrite(NeuralNetPtr,char*);
+void	*nn_doget_pattern(NeuralNetPtr a,patternSetPtr b,char* v);
+void	*nn_doread(NeuralNetPtr a,char* b);
+void	*nn_dowrite(NeuralNetPtr a,char* b);
 #endif
-void	*nn_dqfn(NeuralNetPtr);
-void	*nn_dump_test(NeuralNetPtr);
-void	*nn_dump_train(NeuralNetPtr);
-void	*nn_dump_weights(NeuralNetPtr);
-void	*nn_dump_pattern(NeuralNetPtr,patternSetPtr);
-void	*nn_error_stats(NeuralNetPtr,patternSetPtr,int);
-void	*nn_error_tolerance(NeuralNetPtr,double);
-double	euclideanDist2(double*,double*,int);
+void	*nn_dqfn(NeuralNetPtr a);
+void	*nn_dump_test(NeuralNetPtr b);
+void	*nn_dump_train(NeuralNetPtr a);
+void	*nn_dump_weights(NeuralNetPtr a);
+void	*nn_dump_pattern(NeuralNetPtr a,patternSetPtr b);
+void	*nn_error_stats(NeuralNetPtr r,patternSetPtr a,int b);
+void	*nn_error_tolerance(NeuralNetPtr a,double b);
+double	euclideanDist2(double* a,double* b,int c);
 #ifdef MAC
-void	*nn_fields(DialogPtr,int,int,int,int,double,double,double);
+void	*nn_fields(DialogPtr a,int b,int c,int d,int e,double f,double g,double h);
 #endif
-void	*nn_free(NeuralNetPtr);
-void	*nn_free_memory(NeuralNetPtr);
-void	*nn_free_pattern_memory(NeuralNetPtr,patternSetPtr);
-void	*nn_functHid(NeuralNetPtr,Symbol*);
-void	*nn_functOut(NeuralNetPtr,Symbol*);
-int		nn_get_memory(NeuralNetPtr);
-int		nn_get_pattern_memory(NeuralNetPtr,patternSetPtr);
-void	*nn_get_test(NeuralNetPtr,Symbol*);
-void	*nn_get_train(NeuralNetPtr,Symbol*);
-void	*nn_get_weights(NeuralNetPtr,Symbol*);
-void	*nn_graphicsOn(NeuralNetPtr);
-void	*nn_graphicsOff(NeuralNetPtr);
-void	*nn_growParams(NeuralNetPtr);
-void	*nn_hid_slope(NeuralNetPtr,double);
-void	*nn_lambda(NeuralNetPtr,double);
-void	*nn_learningRate(NeuralNetPtr,double);
-void	*nn_out_slope(NeuralNetPtr,double);
-void	*nn_out_slope_learn(NeuralNetPtr,double);
-void	*nn_out_slope_mom(NeuralNetPtr,double);
-void	*nn_qfn(NeuralNetPtr);
-void	*nn_reinitialise_weights(NeuralNetPtr);
-void	*nn_save(NeuralNetPtr,void *);
-void	*nn_save_weights(NeuralNetPtr,Symbol*);
-void	*nn_scroll(NeuralNetPtr);
-void	*nn_status(NeuralNetPtr);
-void	*nn_update(NeuralNetPtr);
-void	*nn_wsize(NeuralNetPtr);
-void	*nn_xValidateTest(NeuralNetPtr);
-void	*nn_xValidateTrain(NeuralNetPtr);
-double	nn_xValidate(NeuralNetPtr,patternSetPtr,int);
+void	*nn_free(NeuralNetPtr a);
+void	*nn_free_memory(NeuralNetPtr a);
+void	*nn_free_pattern_memory(NeuralNetPtr a,patternSetPtr r);
+void	*nn_functHid(NeuralNetPtr a,Symbol* b);
+void	*nn_functOut(NeuralNetPtr a,Symbol* b);
+int		nn_get_memory(NeuralNetPtr a);
+int		nn_get_pattern_memory(NeuralNetPtr r,patternSetPtr a);
+void	*nn_get_test(NeuralNetPtr a,Symbol* b);
+void	*nn_get_train(NeuralNetPtr a,Symbol* b);
+void	*nn_get_weights(NeuralNetPtr a,Symbol* b);
+void	*nn_graphicsOn(NeuralNetPtr a);
+void	*nn_graphicsOff(NeuralNetPtr a);
+void	*nn_growParams(NeuralNetPtr a);
+void	*nn_hid_slope(NeuralNetPtr a,double b);
+void	*nn_lambda(NeuralNetPtr a,double b);
+void	*nn_learningRate(NeuralNetPtr a,double b);
+void	*nn_out_slope(NeuralNetPtr a,double b);
+void	*nn_out_slope_learn(NeuralNetPtr a,double b);
+void	*nn_out_slope_mom(NeuralNetPtr a,double r);
+void	*nn_qfn(NeuralNetPtr a);
+void	*nn_reinitialise_weights(NeuralNetPtr r);
+void	*nn_save(NeuralNetPtr a,void * b);
+void	*nn_save_weights(NeuralNetPtr a,Symbol* b);
+void	*nn_scroll(NeuralNetPtr a);
+void	*nn_status(NeuralNetPtr a);
+void	*nn_update(NeuralNetPtr a);
+void	*nn_wsize(NeuralNetPtr a);
+void	*nn_xValidateTest(NeuralNetPtr r);
+void	*nn_xValidateTrain(NeuralNetPtr a);
+double	nn_xValidate(NeuralNetPtr r,patternSetPtr a,int b);
 
 #ifdef MAC
-Boolean	PlacePict (int, Point);
+Boolean	PlacePict (int a, Point b);
 #endif
-void	*propagate(NNetPtr);
+void	*propagate(NNetPtr a);
 
 #ifdef nver
-short		rand(void);
+short		rand(void a);
 #endif
-double	random(void);
-void	*recognize(NNetPtr,double *,double *,double *);
+double	random(void a);
+void	*recognize(NNetPtr a,double * b,double * c,double * d);
 
-void	*scroll_it(NeuralNetPtr,int,int);
+void	*scroll_it(NeuralNetPtr a,int b,int c);
 #ifdef MAC
-void	*SetDlgTextItem(DialogPtr,int,char *,int);
+void	*SetDlgTextItem(DialogPtr a,int v,char * c,int d);
 #endif
-void	*setHidFunctAll(NNetPtr,int);
-void	*setLambda(NNetPtr,double);
-void	*setLearningRates(NNetPtr,double,double);
-void	*setOutFunctAll(NNetPtr,int);
-void	*setSlopeFactorHid(NNetPtr,int,double);
-void	*setSlopeFactorHidAll(NNetPtr,double);
-void	*setSlopeFactorOut(NNetPtr,int,double);
-void	*setSlopeFactorOutAll(NNetPtr,double);
-void	*setSlopeLearningRate(NNetPtr,double);
-void	*setSlopeMomentum(NNetPtr,double);
-void	*setupGraphics(NeuralNetPtr);
+void	*setHidFunctAll(NNetPtr a,int v);
+void	*setLambda(NNetPtr a,double v);
+void	*setLearningRates(NNetPtr a,double b,double v);
+void	*setOutFunctAll(NNetPtr a,int b);
+void	*setSlopeFactorHid(NNetPtr a,int b,double c);
+void	*setSlopeFactorHidAll(NNetPtr a,double b);
+void	*setSlopeFactorOut(NNetPtr a,int b,double c);
+void	*setSlopeFactorOutAll(NNetPtr a,double b);
+void	*setSlopeLearningRate(NNetPtr a,double b);
+void	*setSlopeMomentum(NNetPtr a,double b);
+void	*setupGraphics(NeuralNetPtr a);
 
-void	*setWeights(NNetPtr,double *,Boolean);
+void	*setWeights(NNetPtr a,double * v,Boolean b);
 
-double	multiplyInputs(unitPtr);
-double	sumInputs(unitPtr);
-double	sumUpStream(unitPtr);
+double	multiplyInputs(unitPtr a);
+double	sumInputs(unitPtr a);
+double	sumUpStream(unitPtr a);
 
-double	targetErrorHidden(unitPtr);
-double	targetErrorOutput(unitPtr,double);
+double	targetErrorHidden(unitPtr a);
+double	targetErrorOutput(unitPtr a,double b);
 
-void	*version(void);
+void	*version(void );
 
-void	*segment1(void),*segment2(void);
+void	*segment1(void ) ,*segment2(void);
