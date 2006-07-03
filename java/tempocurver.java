82ea7a193e7a288dbd915fe29757764356923778
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2006-07-03 23:12:20 +0000

    Fixed bug, added copyright notice

--- java/tempocurver.java
+++ java/tempocurver.java
@@ -1,3 +1,33 @@
+/*
+Copyright (c) 2006.  
+The Regents of the University of California (Regents).
+All Rights Reserved.
+
+Permission to use, copy, modify, and distribute this software and its
+documentation for educational, research, and not-for-profit purposes, without
+fee and without a signed licensing agreement, is hereby granted, provided that
+the above copyright notice, this paragraph and the following two paragraphs
+appear in all copies, modifications, and distributions.  Contact The Office of
+Technology Licensing, UC Berkeley, 2150 Shattuck Avenue, Suite 510, Berkeley,
+CA 94720-1620, (510) 643-7201, for commercial licensing opportunities.
+
+Written by Matt Wright, The Center for New Music and Audio Technologies,
+University of California, Berkeley.
+
+     IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+     SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+     ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+     REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+     REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+     LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+     FOR A PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING
+     DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS".
+     REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+     ENHANCEMENTS, OR MODIFICATIONS.
+
+*/
+
 import com.cycling74.max.*;
 import com.cycling74.msp.*;
 
@@ -57,11 +87,7 @@ public class tempocurver extends MSPPerformer {
 	}
 
 	public void list(float target_tempo, float target_phase, float time_to_get_there) {
-
-
-		// ``4
-
-``post("you asked for " + target_tempo + " " + target_phase + " " + time_to_get_there);
+		// post("you asked for " + target_tempo + " " + target_phase + " " + time_to_get_there);
 
 		while(target_phase>1.0) {
 			target_phase -= 1.0;
