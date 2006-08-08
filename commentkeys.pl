d56e02f4e07ae03e9f13a439f93a47d540832334
Andy Schmeder <Andy Schmeder@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2006-08-08 23:51:35 +0000

    fix commentkeys.pl on windows; mishandling of extended ascii characters

--- commentkeys.pl
+++ commentkeys.pl
@@ -47,15 +47,17 @@ sub version_sort {
 while($source =~ m~/\*((\n|.)*?)\*/~g) {
     my $block_comment = $1;
     while($block_comment =~ m~\s*\@\@+\s*\n?((\n|.)*?)\n?\s*\@\@+~mg) {
-        my $info_block = $1;
-	while($info_block =~ m~\s*\*?\s*([A-Za-z0-9_\. -]*)\s*?:[ ]*([^\n]*)\s*~mg) {
+      my $info_block = $1;
+	while($info_block =~ m~\s*([A-Za-z0-9_\. -]*)\s*?:\s*([^\n]*)\s*~mg) {
 	    my ($key, $value) = ($1, $2);
 	    $value =~ s~\\~\\\\~g;
 	    $value =~ s~"~\\"~g;
 	    if($key =~ /^VERSION(\s|-)*(.*)$/i) {
 		push @versions, $2;
 	    }
-	    print qq~$key: "$value"\n~; 
+          $key =~ s/[^\x20-\x7e]//;    # remove any weird control characters that might have snuck in!
+          $value =~ s/[^\x20-\x7e]//;
+          print qq~$key: "$value"\n~;
 	}
     }
 }
