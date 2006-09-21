61aac74a4f83c99da02362cdde36809f5d010293
Matt Wright <Matt Wright@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2006-09-21 20:43:54 +0000

    Added proper copyright info and name/value table

--- js/shepard/shepard.js
+++ js/shepard/shepard.js
@@ -1,9 +1,41 @@
+/* 
+Shepard tone synthesis 1.0
+
+Written by Adrian Freed, The Center for New Music and Audio Technologies,
+University of California, Berkeley.  Copyright (c) 2005-6, The Regents of 
+the University of California (Regents).  
+
+Permission to use, copy, modify, distribute, and distribute modified versions
+of this software and its documentation without fee and without a signed
+licensing agreement, is hereby granted, provided that the above copyright
+notice, this paragraph and the following two paragraphs appear in all copies,
+modifications, and distributions.
+
+IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS
+BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+NAME: Shepard
+DESCRIPTION: Shepard tone (infinite glissando) synthesis
+AUTHORS: Adrian Freed
+COPYRIGHT_YEARS: 2005-6
+SVN_REVISION: $LastChangedRevision: 618 $
+VERSION 1.0: First release
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+*/
+
+
 inlets=1;
 outlets=1;
-//Shepard tone synthesis 1.0 
-//Adrian Freed 2005,2006 Copyright UC REgents. All Rights Reserved.
-// 
-
 
 var n=8; // number of partials
 var f0=80; // frequency f0 in Hz
