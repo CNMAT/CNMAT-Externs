ce0b0ea49a7da14938715f23f4f29c55abddcc70
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2006-10-18 17:56:17 +0000

    Changed the xcode project settings to work in svn.  Added license to roughness.c

--- externals/misc/roughness/roughness.c
+++ externals/misc/roughness/roughness.c
@@ -1,4 +1,44 @@
+/*
+Copyright (c) 2006.  The Regents of the University of California (Regents).
+All Rights Reserved.
+
+Permission to use, copy, modify, and distribute this software and its
+documentation for educational, research, and not-for-profit purposes, without
+fee and without a signed licensing agreement, is hereby granted, provided that
+the above copyright notice, this paragraph and the following two paragraphs
+appear in all copies, modifications, and distributions.  Contact The Office of
+Technology Licensing, UC Berkeley, 2150 Shattuck Avenue, Suite 510, Berkeley,
+CA 94720-1620, (510) 643-7201, for commercial licensing opportunities.
+
+Written by John MacCallum, The Center for New Music and Audio Technologies,
+University of California, Berkeley.
+
+     IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+     SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+     ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+     REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+     REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+     LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+     FOR A PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING
+     DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS".
+     REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+     ENHANCEMENTS, OR MODIFICATIONS.
+
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+NAME: roughness
+DESCRIPTION: Roughness estimate based on Richard Parncutt's algorithm.
+AUTHORS: John MacCallum
+COPYRIGHT_YEARS: 2006
+SVN_REVISION: $LastChangedRevision: 587 $
+VERSION 1.0: First version
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
+
+#include "version.h"
 #include "ext.h"
+#include "version.c"
 #include "math.h"
 
 #define R_MAX_LEN 8192
@@ -29,6 +69,9 @@ int main(void)
 {
 	setup((t_messlist **)&rho_class, (method)rho_new, (method)rho_free, (short)sizeof(t_rho), 0L, 0); 
 	
+	version(0);
+
+	addmess((method) version, "version", 0);
 	addbang((method)rho_bang);
 	addmess((method)rho_anything, "anything", A_GIMME, 0);
 	addmess((method)rho_list, "list", A_GIMME, 0);
