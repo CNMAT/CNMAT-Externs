b86002dc779d8c9a6a2ea93d9676485373e7c09d
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2007-03-21 00:16:45 +0000

    John MacCallum added a second outlet to output the length of the list and rewrote main from void main(fptr *f) to int main(void) to keep xcode from complaining.

--- externals/lists/list-accum/list-accum.c
+++ externals/lists/list-accum/list-accum.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2000,01,02,03,04,05,06.  The Regents of the University of California (Regents).
+Copyright (c) 2000,01,02,03,04,05,06,07.  The Regents of the University of California (Regents).
 All Rights Reserved.
 
 Permission to use, copy, modify, and distribute this software and its
@@ -38,6 +38,7 @@ VERSION 0.3: Modified 8/12/05 by Matt Wright to compile again
 VERSION 0.4: Accepts single floats or integers
 VERSION 0.5: Uses sysmem_newptr instead of getbytes(); works with more than 4096
 VERSION 0.5.1: Force Package Info Generation
+VERSION 0.6: MacCallum added a second outlet to output the length of the list.  Also changed main to return an int and take 0 args.
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 */
 
@@ -61,6 +62,7 @@ void *class;
 struct listAccum {
 	struct object l_ob;
 	void   *l_outlet;
+	void   *l_outlet2; //to output the length of the list
 	
 	long	l_numAtoms;
 	long	l_atomsInBuf;
@@ -81,6 +83,7 @@ struct listAccum *listAccumNew(long n) {
 	}
 	
 	x = (struct listAccum *)newobject(class);
+	x->l_outlet2 = intout(x);
 	x->l_outlet = outlet_new(x,0L);
 	x->l_numAtoms = n;
 	x->l_atoms	= (Atom *) sysmem_newptr((long)(sizeof(Atom)*x->l_numAtoms));
@@ -105,8 +108,10 @@ void listAccumBang(struct listAccum *x)
 	if (x->debug)
 		post("listAccum bang");
 	
-	if (x->l_atomsInBuf)
+	if (x->l_atomsInBuf){
+		outlet_int(x->l_outlet2, x->l_atomsInBuf);
 		outlet_list(x->l_outlet,0L,(short)x->l_atomsInBuf,x->l_atoms);
+	}
 	x->l_atomsInBuf = 0;
 }
 
@@ -191,7 +196,7 @@ void listAccum_anything (struct listAccum *x, Symbol *s, short argc, Atom *argv)
 }
 
 
-void main(fptr *f) {		
+int main(void) {		
 	setup((t_messlist **)&class, (method) listAccumNew, (method)listAccumFree,
 		  (short) sizeof(struct listAccum), 0L, A_LONG, 0);
 
@@ -205,4 +210,5 @@ void main(fptr *f) {
 	finder_addclass("Data","listAccum");
 	
 	version(0);
+	return 0;
 }
