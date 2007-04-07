7e66b61290e14176f31530bca0326f4206efc300
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2007-04-07 20:20:18 +0000

    Initial import of gendyn~.  This is a port of Nick Collins' supercollider UGens[1-3].

---
+++ mspexternals/generators/gendyn~/version.h
@@ -0,0 +1,18 @@
+#ifndef NAME
+#define NAME "wavelet~"
+#endif
+#ifndef DESCRIPTION
+#define DESCRIPTION "Discrete Wavelet Transform (DWT)"
+#endif
+#ifndef AUTHORS
+#define AUTHORS "John MacCallum"
+#endif
+#ifndef COPYRIGHT_YEARS
+#define COPYRIGHT_YEARS "2006"
+#endif
+#ifndef SVN_REVISION
+#define SVN_REVISION "$LastChangedRevision: 587 $"
+#endif
+#ifndef VERSION
+#define VERSION "1.1.2"
+#endif
