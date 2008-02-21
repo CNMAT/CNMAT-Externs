728a8db458b627cf96a7d0164de93bc47afaed92
Andy Schmeder <Andy Schmeder@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2008-02-21 22:04:48 +0000

    rewrite of arg handler, modes, added proper version.h support

--- mspexternals/misc/accumulate~/accumulate~.c
+++ mspexternals/misc/accumulate~/accumulate~.c
@@ -23,11 +23,11 @@ MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 NAME: accumulate~
-DESCRIPTION: Signal integrator like +=~ with signal control for reset, leakiness and overflow
+DESCRIPTION: Signal integrator with signal-rate reset control, configurable bounds, leakiness and overflow modes
 AUTHORS: Andy Schmeder
-COPYRIGHT_YEARS: 2007
+COPYRIGHT_YEARS: 2008
 SVN_REVISION: $LastChangedRevision$
-VERSION 0.2: New argument syntax
+VERSION 0.2: Added argument syntax, updated mode controls
 VERSION 0.1: First version
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  
 
@@ -40,17 +40,23 @@ VERSION 0.1: First version
 
 #include <math.h>
 #include <float.h>
+#include <string.h>
+
+#define MODE_CLIP 0
+#define MODE_WRAP 1
+#define MODE_RESET 2
 
 void *acc_class;
 
 typedef struct _t_acc
 {
     t_pxobject x_obj;	//  header
-    float state;
-    float initialValue;
-    float maxValue;
-    long maxMode;
-    float leakP;
+    float current;
+    float start;
+    float max;
+    float min;
+    long  mode;
+    float scale;
 } t_acc;
 
 int main(void);
@@ -61,8 +67,7 @@ t_int *acc_perform(t_int *w);
 void acc_free(t_acc *x);
 
 int main(void) {
-	post("accumulate~ version " ACCUMULATE_VERSION " by Andy Schmeder");
-	post("Copyright © 2007 Regents of the University of California.");
+    version(0);
 
     setup((t_messlist **)&acc_class, (method)acc_new, (method)acc_free,
           (short)sizeof(t_acc), 0L, A_GIMME, 0);
@@ -78,13 +83,14 @@ void *acc_new(t_symbol *s, short argc, t_atom *argv) {
     int i;
 	
 	x = (t_acc *)newobject(acc_class);  // dynamic alloc
-  	dsp_setup((t_pxobject *)x, 2);  // two signal inlets
+  	dsp_setup((t_pxobject *)x, 2);      // two signal inlets
     
-    x->state = 0.f;
-    x->initialValue = 0.f;
-    x->maxValue = MAXFLOAT;
-    x->maxMode = 0; // Saturate
-    x->leakP = 0.f;
+    x->current = 0.f;
+    x->start = 0.f;
+    x->max = MAXFLOAT;
+    x->min = -1.0f * MAXFLOAT;
+    x->mode = MODE_CLIP; // Saturate
+    x->scale = 1.0f;
     
 	outlet_new((t_object *)x,"signal");
 
@@ -92,55 +98,114 @@ void *acc_new(t_symbol *s, short argc, t_atom *argv) {
 
         for(i = 0; i < argc; i++) {
 
-            switch (i) {
-            
-                case 0: // initial value
-                    if (argv[i].a_type == A_FLOAT) {
-                        x->initialValue = atom_getfloatarg(i,argc,argv);
-                        x->state = x->initialValue;
-                        //post("initial value: %f", x->initialValue);
-                    } else {
-                        //post("expected to get a float for argument #1");
-                    }
-                    break;
-                case 1: // max value
-                    if (argv[i].a_type == A_FLOAT) {
-                        x->maxValue = atom_getfloatarg(i,argc,argv);
-                        //post("max value is %f", x->maxValue);
-                    } // otherwise just ignore it (inf or whatever)
-                    else { 
-                        //post("max value inf"); 
-                    }
-                    break;
-                case 2: // max mode
-                    if (argv[i].a_type == A_LONG) {
-                        x->maxMode = atom_getintarg(i,argc,argv);
-                        if(x->maxMode < 0 || x->maxMode > 2) {
-                            post("invalid mode! valid modes are 0, 1, 2.");
+            if(argv[i].a_type == A_SYM) {
+                
+                if(strcmp(argv[i].a_w.w_sym->s_name, "@start")) {
+                    if(i + 1 < argc) {
+                        i++;
+                        
+                        if(argv[i].a_type == A_FLOAT) {
+                            x->start = atom_getfloatarg(i,argc,argv);
                         }
-                        //post("mode %d", x->maxMode);
+                        else if(argv[i].a_type == A_LONG) {
+                            x->start = atom_getintarg(i,argc,argv);
+                        }
+                        else {
+                            post("accumulate~: expected float or long after @start");
+                        }
                     } else {
-                        //post("expected to get an int for argument #3");
+                        post("accumulate~: expected an arg after @start");
                     }
-                    break;
-                case 3: // leak constant
-                    if (argv[i].a_type == A_FLOAT) {
-                        x->leakP = 1.f - atom_getfloatarg(i,argc,argv);
-                        //post("leakiness %f", x->leakP);
+                }
+                
+                else if(strcmp(argv[i].a_w.w_sym->s_name, "@max")) {
+                    if(i + 1 < argc) {
+                        i++;
+                        
+                        if(argv[i].a_type == A_FLOAT) {
+                            x->max = atom_getfloatarg(i,argc,argv);
+                        }
+                        else if(argv[i].a_type == A_LONG) {
+                            x->max = atom_getintarg(i,argc,argv);
+                        }
+                        else {
+                            post("accumulate~: expected float or long after @max");
+                        }
                     } else {
-                        //post("expected to get a float for argument #4");
+                        post("accumulate~: expected an arg after @max");
                     }
-                    break;
-                default:
-                    //post("unexpected extra arguments!");
+                }
+
+                else if(strcmp(argv[i].a_w.w_sym->s_name, "@min")) {
+                    if(i + 1 < argc) {
+                        i++;
+                        
+                        if(argv[i].a_type == A_FLOAT) {
+                            x->min = atom_getfloatarg(i,argc,argv);
+                        }
+                        else if(argv[i].a_type == A_LONG) {
+                            x->min = atom_getintarg(i,argc,argv);
+                        }
+                        else {
+                            post("accumulate~: expected float or long after @min");
+                        }
+                    } else {
+                        post("accumulate~: expected an arg after @max");
+                    }
+                }
+                
+                else if(strcmp(argv[i].a_w.w_sym->s_name, "@scale")) {
+                    if(i + 1 < argc) {
+                        i++;
+                        
+                        if(argv[i].a_type == A_FLOAT) {
+                            x->scale = atom_getfloatarg(i,argc,argv);
+                        }
+                        else if(argv[i].a_type == A_LONG) {
+                            x->scale = atom_getintarg(i,argc,argv);
+                        }
+                        else {
+                            post("accumulate~: expected float or long after @scale");
+                        }
+                    } else {
+                        post("accumulate~: expected an arg after @scale");
+                    }
+                }
+                
+                else if(strcmp(argv[i].a_w.w_sym->s_name, "@mode")) {
+                    if(i + 1 < argc) {
+                        i++;
+
+                        if(argv[i].a_type == A_SYM) {
+                            if(strcmp(argv[i].a_w.w_sym->s_name, "clip") == 0) {
+                                x->mode = MODE_CLIP;
+                            } else if(strcmp(argv[i].a_w.w_sym->s_name, "wrap") == 0) {
+                                x->mode = MODE_WRAP;
+                            } else if(strcmp(argv[i].a_w.w_sym->s_name, "reset") == 0) {
+                                x->mode = MODE_RESET;
+                            } else {
+                                post("accumulate~: unknown @mode %s", argv[i].a_w.w_sym->s_name);
+                            }
+                        } else {
+                            post("accumulate~: expected symbol after @mode");
+                        }
+                    } else {
+                        post("accumulate~: missing argument after @mode");
+                    }
+                }
+                
+                else {
+                    
+                    post("accumulate~: unexpected arguments");
+                    
+                }
+                
             }
             
         }
         
-    } else {
-        //post("default operation mode");
     }
-	
+    
     return x;
 }
 
@@ -176,7 +241,7 @@ t_int *acc_perform(t_int *w) {
 
     // here the loop is copied three times for each case to avoid unnecessary conditions in the inner loop
     
-    if (x->maxMode == 0) { // no maximum.
+    if (x->mode == MODE_CLIP) {
         
         while (size--) {
         
@@ -185,17 +250,17 @@ t_int *acc_perform(t_int *w) {
 
             // reset by control signal
             if (next_control == 0.) {
-                x->state = x->initialValue;
+                x->current = x->start;
             }
             
-            x->state += next_signal;
-
-            *out++ = x->state;
-
-            x->state = x->state * x->leakP; // apply leaky integrator
+            x->current = fmaxf(fminf(x->current + next_signal, x->max), x->min);
+            
+            *out++ = x->current;
+            
+            x->current = x->current * x->scale; // apply leaky integrator with one-sample delay
         }
         
-	} else if (x->maxMode == 1) { // saturation at maximum
+	} else if (x->mode == MODE_WRAP) { // saturation at maximum
             
         while (size--) {
 
@@ -204,19 +269,26 @@ t_int *acc_perform(t_int *w) {
             
             // reset by control signal
             if (next_control == 0.) {
-                x->state = x->initialValue;
+                x->current = x->start;
             }
             
-            x->state += next_signal;
+            x->current += next_signal;
             
-            x->state = fminf(x->state, x->maxValue);
-            *out++ = x->state;
+            // wrap top and bottom boundary
+            if(x->current > x->max) {
+                x->current = x->min;
+            }
+            else if(x->current < x->min) {
+                x->current = x->max;
+            }
             
-            x->state = x->state * x->leakP; // apply leaky integrator
+            *out++ = x->current;
+            
+            x->current = x->current * x->scale; // apply leaky integrator
             
         }
         
-    } else if (x->maxMode == 2) {
+    } else if (x->mode == MODE_RESET) {
         
         while (size--) {
             
@@ -225,16 +297,18 @@ t_int *acc_perform(t_int *w) {
             
             // reset by control signal
             if (next_control == 0.) {
-                x->state = x->initialValue;
+                x->current = x->start;
             }
             
-            x->state += next_signal;
+            x->current += next_signal;
             
-            x->state = fmodf(x->state + next_signal, x->maxValue);
+            if(x->current > x->max || x->current < x->min) {
+                x->current = x->start;
+            }
             
-            *out++ = x->state;
+            *out++ = x->current;
 
-            x->state = x->state * x->leakP; // apply leaky integrator
+            x->current = x->current * x->scale; // apply leaky integrator
         }
         
     }
