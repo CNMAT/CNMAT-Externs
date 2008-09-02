13ec91f1691fe4d33fb8259a8a91a1f9ba8a20e6
Michael Zbyszynski <Michael Zbyszynski@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2008-09-02 21:00:41 +0000

    adapting to release through CNMAT

--- mspexternals/analysis/beat~/beat~.c
+++ mspexternals/analysis/beat~/beat~.c
@@ -2,6 +2,46 @@
 /* Written by Tristan Jehan at the MIT Media Laboratory */
 /* Copyright MIT, all rights reserved */
 
+/*
+ beat~ object by Tristan Jehan
+ Copyright (c) 2004 Masachussetts Institute of Technology.
+ All rights reserved.
+ 
+ Permission to use, copy, modify, and distribute this software and its
+ documentation for educational, research, and not-for-profit purposes, without
+ fee and without a signed licensing agreement, is hereby granted, provided that
+ the above copyright notice, this paragraph and the following two paragraphs
+ appear in all copies, modifications, and distributions.  Contact The Office of
+ Technology Licensing, UC Berkeley, 2150 Shattuck Avenue, Suite 510, Berkeley,
+ CA 94720-1620, (510) 643-7201, for commercial licensing opportunities.
+  
+ IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+ SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+ ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE. THE SOFTWARE AND ACCOMPANYING
+ DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED "AS IS".
+ REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
+ ENHANCEMENTS, OR MODIFICATIONS.
+ 
+ beat.c
+ 
+ 
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ NAME: beat~
+ DESCRIPTION: Beat analyzer of an arbitrary audio channel
+ AUTHORS: Tristan Jehan
+ COPYRIGHT_YEARS: 2004
+ DRUPAL_NODE: /patch/xxxx
+ SVN_REVISION: $LastChangedRevision: 1916 $
+ version 1.0.1: Tristans version.
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ 
+ */
+
 #include "ext.h"
 #include "z_dsp.h"
 #include "fft.h"
