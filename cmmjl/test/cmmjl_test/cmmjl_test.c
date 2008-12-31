4ae2bd5911308ca639db26a19ea9207f6ee5adc8
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2008-12-31 02:40:19 +0000

    added fullpacket stuff and updated the obj to use the new instiantiation methods in the max5 sdk

--- cmmjl/test/cmmjl_test/cmmjl_test.c
+++ cmmjl/test/cmmjl_test/cmmjl_test.c
@@ -1,12 +1,23 @@
+/*
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ NAME: cmmjl_test
+ DESCRIPTION: Testing object for the cmmjl
+ AUTHORS: John MacCallum
+ COPYRIGHT_YEARS: 2008
+ SVN_REVISION: $LastChangedRevision: 587 $
+ VERSION 1.0: First version
+ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ */
 //#define DEBUG
 
 #include "version.h"
 #include "ext.h"
+#include "ext_obex.h"
 #include "version.c"
-#include "cmmjl/cmmjl.h"
-#include "cmmjl/cmmjl_commonsymbols.h"
-#include "cmmjl/cmmjl_error.h"
-#include "cmmjl/cmmjl_osc.h"
+#include "cmmjl/cmmjl.h" /* Include this for basic functionality */
+#include "cmmjl/cmmjl_commonsymbols.h" /* predefined t_symbols */
+#include "cmmjl/cmmjl_error.h" /* error reporting */
+#include "cmmjl/cmmjl_osc.h" /* OSC support */
 
 typedef struct _test{
 	t_object t_ob;
@@ -17,22 +28,37 @@ void *test_class;
 
 void *test_new();
 void test_free(t_test *x);
+
+/* This will be called in response to the FullPacket message */
 void test_fullpacket(t_test *x, long len, long ptr);
+/* This will be called by cmmjl_osc_parseFullPacket as each message is parsed out of the OSC packet */
 void test_fullpacket_callback(void *x, t_symbol *sym, int argc, t_atom *argv);
+void test_bar(t_test *x, long l);
 
 int main(int argc, char **argv){
-	setup((t_messlist **)&test_class, (method)test_new, (method)test_free, (short)sizeof(t_test), 0L, 0);
-	addmess((method)test_fullpacket, "FullPacket", A_LONG, A_LONG, 0);
+	t_class *c;
+	c = class_new("cmmjl_test", (method)test_new, (method)test_free, (long)sizeof(t_test), 0L, 0);
+	class_addmethod(c, (method)test_bar, "bar", A_LONG, 0);
 	
+	/* Initialize error reporter, symbol table, etc. */
+	cmmjl_init();
+	/* Accept the FullPacket message and set the callback */
+	CMMJL_ACCEPT_FULLPACKET(c, test_fullpacket);
+	
+	int bloo = 21;
+
+	/* report an error like this */
+	CMMJL_ERROR(19, "some foo screwed up here making bar = %d", bloo);
+
+	class_register(CLASS_BOX, c);
+	test_class = c;
+
 	return 0;
 }
 
 void *test_new(){
-	t_test *x;
-	x = (t_test *)newobject(test_class);
+	t_test *x = (t_test *)object_alloc(test_class);
 	x->outlet = outlet_new(x, 0);
-
-	cmmjl_init();
 
 	return x;
 }
@@ -41,10 +67,20 @@ void test_free(t_test *x){
 }
 
 void test_fullpacket(t_test *x, long len, long ptr){
+	/* Debugging is done with PDEBUG */
 	PDEBUG("len = %d, ptr = %d", len, ptr);
+	/* When we get a pointer to a packet, call this function with the callback that
+	   will handle the messages as they are parsed out of the packet. */
 	t_cmmjl_error e = cmmjl_osc_parseFullPacket(x, (char *)ptr, len, true, test_fullpacket_callback);
 }
 
 void test_fullpacket_callback(void *x, t_symbol *sym, int argc, t_atom *argv){
+	t_atom *out = NULL;
+	t_max_err r = object_method_typed(x, gensym(basename(sym->s_name)), argc, argv, out);
+	post("r = %d", r);
 	outlet_anything(((t_test *)x)->outlet, sym, argc, argv);
+}
+
+void test_bar(t_test *x, long l){
+	post("l = %d");	
 }
--- cmmjl/test/cmmjl_test/cmmjl_test.maxhelp
+++ cmmjl/test/cmmjl_test/cmmjl_test.maxhelp
@@ -18,14 +18,29 @@
 		"metadata" : [  ],
 		"boxes" : [ 			{
 				"box" : 				{
+					"maxclass" : "message",
+					"text" : "/bar 19, bang",
+					"fontsize" : 12.0,
+					"patching_rect" : [ 115.0, 76.0, 81.0, 18.0 ],
+					"outlettype" : [ "" ],
+					"id" : "obj-2",
+					"fontname" : "Arial",
+					"numinlets" : 2,
+					"presentation_rect" : [ 115.0, 76.0, 0.0, 0.0 ],
+					"numoutlets" : 1
+				}
+
+			}
+, 			{
+				"box" : 				{
 					"maxclass" : "newobj",
 					"text" : "print",
+					"fontsize" : 12.0,
+					"patching_rect" : [ 205.0, 238.0, 34.0, 20.0 ],
 					"id" : "obj-5",
 					"fontname" : "Arial",
 					"numinlets" : 1,
-					"numoutlets" : 0,
-					"fontsize" : 12.0,
-					"patching_rect" : [ 205.0, 238.0, 34.0, 20.0 ]
+					"numoutlets" : 0
 				}
 
 			}
@@ -33,13 +48,13 @@
 				"box" : 				{
 					"maxclass" : "message",
 					"text" : "/foo 19, bang",
+					"fontsize" : 12.0,
+					"patching_rect" : [ 205.0, 103.0, 81.0, 18.0 ],
+					"outlettype" : [ "" ],
 					"id" : "obj-4",
 					"fontname" : "Arial",
 					"numinlets" : 2,
-					"numoutlets" : 1,
-					"fontsize" : 12.0,
-					"outlettype" : [ "" ],
-					"patching_rect" : [ 205.0, 103.0, 81.0, 18.0 ]
+					"numoutlets" : 1
 				}
 
 			}
@@ -47,13 +62,13 @@
 				"box" : 				{
 					"maxclass" : "newobj",
 					"text" : "OpenSoundControl",
+					"fontsize" : 12.0,
+					"patching_rect" : [ 205.0, 131.0, 113.0, 20.0 ],
+					"outlettype" : [ "", "", "OSCTimeTag" ],
 					"id" : "obj-1",
 					"fontname" : "Arial",
 					"numinlets" : 1,
-					"numoutlets" : 3,
-					"fontsize" : 12.0,
-					"outlettype" : [ "", "", "OSCTimeTag" ],
-					"patching_rect" : [ 205.0, 131.0, 113.0, 20.0 ]
+					"numoutlets" : 3
 				}
 
 			}
@@ -61,21 +76,30 @@
 				"box" : 				{
 					"maxclass" : "newobj",
 					"text" : "cmmjl_test",
+					"fontsize" : 12.0,
+					"patching_rect" : [ 205.0, 198.0, 68.0, 20.0 ],
+					"outlettype" : [ "" ],
 					"id" : "obj-3",
 					"fontname" : "Arial",
 					"numinlets" : 1,
-					"numoutlets" : 1,
-					"fontsize" : 12.0,
-					"outlettype" : [ "" ],
-					"patching_rect" : [ 205.0, 198.0, 68.0, 20.0 ]
+					"numoutlets" : 1
 				}
 
 			}
  ],
 		"lines" : [ 			{
 				"patchline" : 				{
-					"source" : [ "obj-3", 0 ],
-					"destination" : [ "obj-5", 0 ],
+					"source" : [ "obj-2", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-4", 0 ],
+					"destination" : [ "obj-1", 0 ],
 					"hidden" : 0,
 					"midpoints" : [  ]
 				}
@@ -92,8 +116,8 @@
 			}
 , 			{
 				"patchline" : 				{
-					"source" : [ "obj-4", 0 ],
-					"destination" : [ "obj-1", 0 ],
+					"source" : [ "obj-3", 0 ],
+					"destination" : [ "obj-5", 0 ],
 					"hidden" : 0,
 					"midpoints" : [  ]
 				}
