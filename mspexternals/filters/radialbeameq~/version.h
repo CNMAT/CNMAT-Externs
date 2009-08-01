ba1fa0c60142cd8b2830b2a2a15cc58a45bd7dca
Andy Schmeder <Andy Schmeder@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2009-08-01 01:37:49 +0000

    radialbeameq added

---
+++ mspexternals/filters/radialbeameq~/version.h
@@ -0,0 +1,18 @@
+#ifndef NAME
+#define NAME "radialbeameq~"
+#endif
+#ifndef DESCRIPTION
+#define DESCRIPTION "IIR filter cascade for radial equalization of a dirac beam"
+#endif
+#ifndef AUTHORS
+#define AUTHORS "Franz Zotter, Andy Schmeder"
+#endif
+#ifndef COPYRIGHT_YEARS
+#define COPYRIGHT_YEARS "2009"
+#endif
+#ifndef SVN_REVISION
+#define SVN_REVISION "$LastChangedRevision: 550 $"
+#endif
+#ifndef VERSION
+#define VERSION "0.1"
+#endif
