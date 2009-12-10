efea027927f77ae6bcfee3bb73963d7c66ac7738
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2009-12-10 02:01:22 +0000

    added bdist to the repository

---
+++ externals/misc/bdist/bdist.c
@@ -0,0 +1,292 @@
+/*
+  Written by John MacCallum, The Center for New Music and Audio Technologies,
+  University of California, Berkeley.  Copyright (c) 2009, The Regents of
+  the University of California (Regents). 
+  Permission to use, copy, modify, distribute, and distribute modified versions
+  of this software and its documentation without fee and without a signed
+  licensing agreement, is hereby granted, provided that the above copyright
+  notice, this paragraph and the following two paragraphs appear in all copies,
+  modifications, and distributions.
+
+  IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+  SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+  OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS
+  BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+  REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+  PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+  HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
+  MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  NAME: bdist
+  DESCRIPTION: The beta distribution
+  AUTHORS: John MacCallum
+  COPYRIGHT_YEARS: 2009
+  SVN_REVISION: $LastChangedRevision: 587 $
+  VERSION 0.0: First try
+  VERSION 0.0.1: Added version info
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
+
+#include "version.h"
+#include "ext.h"
+#include "ext_obex.h"
+#include "ext_obex_util.h"
+#include "jpatcher_api.h" 
+#include "jgraphics.h"
+#include "ext_critical.h"
+#include "version.c"
+#include "math.h"
+
+#include "gsl/gsl_sf.h"
+#include "gsl/gsl_errno.h"
+
+#define DEF_BUF_LEN 128
+
+typedef struct _bdist{
+	t_jbox ob;
+	void *outlet;
+	void *proxies[2];
+	long inlet;
+	t_critical lock;
+	t_jrgba bgcolor, linecolor, bordercolor;
+	double x, a, b;
+	double xmin, xmax, ymin, ymax;
+} t_bdist;
+
+static t_class *bdist_class;
+
+double bdist_beta(double x, double a, double b);
+void bdist_paint(t_bdist *x, t_object *patcherview);
+void bdist_bang(t_bdist *x);
+void bdist_int(t_bdist *x, long n);
+void bdist_float(t_bdist *x, double xx);
+void bdist_list(t_bdist *x, t_symbol *msg, short argc, t_atom *argv);
+void bdist_free(t_bdist *x);
+void bdist_assist(t_bdist *x, void *b, long m, long a, char *s);
+double bdist_scale(double f, double min_in, double max_in, double min_out, double max_out);
+double bdist_clip(double f, double min, double max);
+void *bdist_new(t_symbol *msg, int argc, t_atom *argv);
+
+double bdist_beta(double x, double a, double b){
+	return gsl_sf_beta_inc(a, b, bdist_clip(x, 0., 1.));
+}
+
+void bdist_paint(t_bdist *x, t_object *patcherview){
+	t_rect rect;
+    
+	t_jgraphics *g = (t_jgraphics *)patcherview_get_jgraphics(patcherview);
+	jbox_get_rect_for_view((t_object *)x, patcherview, &rect);
+        
+	jgraphics_set_source_jrgba(g, &(x->bordercolor));
+	jgraphics_set_line_width(g, 1);
+	jgraphics_rectangle(g, 0., 0., rect.width, rect.height);
+	jgraphics_stroke(g);
+    
+	jgraphics_set_source_jrgba(g, &(x->bgcolor));
+	jgraphics_rectangle(g, 0., 0., rect.width, rect.height);
+	jgraphics_fill(g);
+
+	// draw the curve
+	{
+		jgraphics_set_source_jrgba(g, &(x->linecolor));
+		int i;
+		jgraphics_move_to(g, 0, rect.height);
+		for(i = 1; i < rect.width; i++){
+			jgraphics_line_to(g, i, rect.height * (1. - bdist_beta(i / rect.width, x->a, x->b)));
+		}
+		jgraphics_stroke(g);
+	}
+
+	// draw the crosshairs
+	{
+		double f = rect.height * (1. - bdist_beta(x->x, x->a, x->b));
+		jgraphics_set_source_jrgba(g, &(x->linecolor));
+		jgraphics_move_to(g, 0, f);
+		jgraphics_line_to(g, rect.width, f);
+		f = rect.width * x->x;
+		jgraphics_move_to(g, f, 0);
+		jgraphics_line_to(g, f, rect.height);
+		jgraphics_stroke(g);
+	}
+}
+
+void bdist_bang(t_bdist *x){
+}
+
+void bdist_int(t_bdist *x, long n){
+}
+
+void bdist_float(t_bdist *x, double f){
+	switch(proxy_getinlet((t_object *)x)){
+	case 0:
+		x->x = bdist_scale(f, x->xmin, x->xmax, 0., 1.);
+		x->x = bdist_clip(x->x, 0., 1.);
+		outlet_float(x->outlet, bdist_scale(bdist_beta(x->x, x->a, x->b), 0., 1., x->ymin, x->ymax));
+		break;
+	case 1:
+		x->a = bdist_clip(f, DBL_MIN, DBL_MAX);
+		break;
+	case 2:
+		x->b = bdist_clip(f, DBL_MIN, DBL_MAX);
+		break;
+	}
+	jbox_redraw(&(x->ob));
+}
+
+void bdist_list(t_bdist *x, t_symbol *msg, short argc, t_atom *argv){
+}
+
+void bdist_anything(t_bdist *x, t_symbol *msg, short argc, t_atom *argv){
+
+}
+
+void bdist_free(t_bdist *x){
+	jbox_free(&(x->ob));
+	critical_free(x->lock);
+}
+
+void bdist_assist(t_bdist *x, void *b, long io, long index, char *s){
+	switch(io){
+	case 1:
+		switch(index){
+		case 0:
+			sprintf(s, "");
+			break;
+		}
+		break;
+	case 2:
+		switch(index){
+		case 0:
+			sprintf(s, "");
+			break;
+		}
+		break;
+	}
+}
+
+double bdist_scale(double f, double min_in, double max_in, double min_out, double max_out){
+	float m = (max_out - min_out) / (max_in - min_in);
+	float b = (min_out - (m * min_in));
+	return m * f + b;
+}
+
+double bdist_clip(double f, double min, double max){
+	double ff = f;
+	if(ff < min){
+		ff = min;
+	}
+	if(ff > max){
+		ff = max;
+	}
+	return ff;
+}
+
+
+/**************************************************
+Object and instance creation functions.
+**************************************************/
+
+void *bdist_new(t_symbol *msg, int argc, t_atom *argv){
+	t_bdist *x;
+    
+ 	t_dictionary *d = NULL; 
+ 	long boxflags; 
+    
+	// box setup 
+	if(!(d = object_dictionaryarg(argc, argv))){ 
+		return NULL; 
+	} 
+    
+	boxflags = 0 
+		| JBOX_DRAWFIRSTIN 
+		//| JBOX_NODRAWBOX
+		| JBOX_DRAWINLAST
+		//| JBOX_TRANSPARENT  
+		//      | JBOX_NOGROW
+		//| JBOX_GROWY
+		| JBOX_GROWBOTH
+		//      | JBOX_HILITE
+		| JBOX_BACKGROUND
+		| JBOX_DRAWBACKGROUND
+		//      | JBOX_NOFLOATINSPECTOR
+		//      | JBOX_MOUSEDRAGDELTA
+		//      | JBOX_TEXTFIELD
+		;
+
+	if(x = (t_bdist *)object_alloc(bdist_class)){
+		jbox_new((t_jbox *)x, boxflags, argc, argv); 
+ 		x->ob.b_firstin = (void *)x; 
+
+		x->outlet = outlet_new(x, NULL);
+		x->proxies[1] = proxy_new((t_object *)x, 2, &(x->inlet));
+		x->proxies[0] = proxy_new((t_object *)x, 1, &(x->inlet));
+
+		critical_new(&(x->lock));
+        
+		attr_dictionary_process(x, d); 
+ 		jbox_ready((t_jbox *)x); 
+        
+		return x;
+	}
+	return NULL;
+}
+
+int main(void){
+	t_class *c = class_new("bdist", (method)bdist_new, (method)bdist_free, sizeof(t_bdist), 0L, A_GIMME, 0);
+
+	c->c_flags |= CLASS_FLAG_NEWDICTIONARY; 
+ 	jbox_initclass(c, JBOX_FIXWIDTH | JBOX_COLOR | JBOX_FONTATTR); 
+    
+	class_addmethod(c, (method)bdist_paint, "paint", A_CANT, 0); 
+	class_addmethod(c, (method)bdist_bang, "bang", 0);
+	class_addmethod(c, (method)bdist_int, "int", A_LONG, 0);
+	class_addmethod(c, (method)bdist_float, "float", A_FLOAT, 0);
+	class_addmethod(c, (method)bdist_list, "list", A_GIMME, 0);
+	class_addmethod(c, (method)bdist_assist, "assist", A_CANT, 0);
+    
+	CLASS_STICKY_ATTR(c, "category", 0, "Color"); 
+    
+ 	CLASS_ATTR_RGBA(c, "bgcolor", 0, t_bdist, bgcolor); 
+ 	CLASS_ATTR_DEFAULTNAME_SAVE_PAINT(c, "bgcolor", 0, "1. 1. 1. 1."); 
+ 	CLASS_ATTR_STYLE_LABEL(c, "bgcolor", 0, "rgba", "Background Color"); 
+
+ 	CLASS_ATTR_RGBA(c, "linecolor", 0, t_bdist, linecolor); 
+ 	CLASS_ATTR_DEFAULTNAME_SAVE_PAINT(c, "linecolor", 0, "0. 0. 0. 1."); 
+ 	CLASS_ATTR_STYLE_LABEL(c, "linecolor", 0, "rgba", "Line Color"); 
+
+ 	CLASS_ATTR_RGBA(c, "bordercolor", 0, t_bdist, bordercolor); 
+ 	CLASS_ATTR_DEFAULTNAME_SAVE_PAINT(c, "bordercolor", 0, "0. 0. 0. 1."); 
+ 	CLASS_ATTR_STYLE_LABEL(c, "bordercolor", 0, "rgba", "Border Color"); 
+    
+	CLASS_STICKY_ATTR_CLEAR(c, "category"); 
+
+	CLASS_ATTR_DOUBLE(c, "a", 0, t_bdist, a);
+	CLASS_ATTR_DEFAULTNAME_SAVE(c, "a", 0, "1.0");
+	CLASS_ATTR_DOUBLE(c, "b", 0, t_bdist, b);
+	CLASS_ATTR_DEFAULTNAME_SAVE(c, "b", 0, "1.0");
+
+	CLASS_ATTR_DOUBLE(c, "xmin", 0, t_bdist, xmin);
+	CLASS_ATTR_DEFAULTNAME_SAVE(c, "xmin", 0, "0.0");
+	CLASS_ATTR_DOUBLE(c, "xmax", 0, t_bdist, xmax);
+	CLASS_ATTR_DEFAULTNAME_SAVE(c, "xmax", 0, "1.0");
+
+	CLASS_ATTR_DOUBLE(c, "ymin", 0, t_bdist, ymin);
+	CLASS_ATTR_DEFAULTNAME_SAVE(c, "ymin", 0, "0.0");
+	CLASS_ATTR_DOUBLE(c, "ymax", 0, t_bdist, ymax);
+	CLASS_ATTR_DEFAULTNAME_SAVE(c, "ymax", 0, "1.0");
+    
+    
+	class_register(CLASS_BOX, c);
+	bdist_class = c;
+
+	common_symbols_init();
+
+	version(0);
+
+	return 0;
+}
+
---
+++ externals/misc/bdist/bdist.maxhelp
@@ -0,0 +1,331 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"rect" : [ 453.0, 44.0, 520.0, 461.0 ],
+		"bglocked" : 0,
+		"defrect" : [ 453.0, 44.0, 520.0, 461.0 ],
+		"openrect" : [ 0.0, 0.0, 0.0, 0.0 ],
+		"openinpresentation" : 0,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 0,
+		"gridsize" : [ 4.0, 4.0 ],
+		"gridsnaponopen" : 0,
+		"toolbarvisible" : 1,
+		"boxanimatetime" : 200,
+		"imprint" : 0,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"boxes" : [ 			{
+				"box" : 				{
+					"maxclass" : "flonum",
+					"fontsize" : 12.0,
+					"outlettype" : [ "float", "bang" ],
+					"patching_rect" : [ 68.0, 176.0, 50.0, 20.0 ],
+					"id" : "obj-5",
+					"fontname" : "Arial",
+					"numinlets" : 1,
+					"numoutlets" : 2
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "comment",
+					"text" : "x",
+					"fontsize" : 12.0,
+					"patching_rect" : [ 64.0, 208.0, 19.0, 20.0 ],
+					"id" : "obj-27",
+					"fontname" : "Arial",
+					"numinlets" : 1,
+					"frgb" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"numoutlets" : 0
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "flonum",
+					"fontsize" : 12.0,
+					"outlettype" : [ "float", "bang" ],
+					"patching_rect" : [ 452.0, 176.0, 50.0, 20.0 ],
+					"id" : "obj-24",
+					"fontname" : "Arial",
+					"numinlets" : 1,
+					"numoutlets" : 2
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "flonum",
+					"fontsize" : 12.0,
+					"outlettype" : [ "float", "bang" ],
+					"patching_rect" : [ 264.0, 176.0, 50.0, 20.0 ],
+					"id" : "obj-23",
+					"fontname" : "Arial",
+					"numinlets" : 1,
+					"numoutlets" : 2
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "message",
+					"text" : "2.",
+					"fontsize" : 12.0,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 392.0, 188.0, 32.5, 18.0 ],
+					"id" : "obj-13",
+					"fontname" : "Arial",
+					"numinlets" : 2,
+					"numoutlets" : 1
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "message",
+					"text" : "1.",
+					"fontsize" : 12.0,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 392.0, 164.0, 32.5, 18.0 ],
+					"id" : "obj-14",
+					"fontname" : "Arial",
+					"numinlets" : 2,
+					"numoutlets" : 1
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "message",
+					"text" : "2.",
+					"fontsize" : 12.0,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 204.0, 188.0, 32.5, 18.0 ],
+					"id" : "obj-12",
+					"fontname" : "Arial",
+					"numinlets" : 2,
+					"numoutlets" : 1
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "message",
+					"text" : "1.",
+					"fontsize" : 12.0,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 204.0, 164.0, 32.5, 18.0 ],
+					"id" : "obj-11",
+					"fontname" : "Arial",
+					"numinlets" : 2,
+					"numoutlets" : 1
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "comment",
+					"text" : "b",
+					"fontsize" : 12.0,
+					"patching_rect" : [ 444.0, 208.0, 19.0, 20.0 ],
+					"id" : "obj-9",
+					"fontname" : "Arial",
+					"numinlets" : 1,
+					"frgb" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"numoutlets" : 0
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "comment",
+					"text" : "a",
+					"fontsize" : 12.0,
+					"patching_rect" : [ 256.0, 204.0, 19.0, 20.0 ],
+					"id" : "obj-8",
+					"fontname" : "Arial",
+					"numinlets" : 1,
+					"frgb" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"numoutlets" : 0
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "flonum",
+					"fontsize" : 12.0,
+					"outlettype" : [ "float", "bang" ],
+					"patching_rect" : [ 40.0, 384.0, 116.0, 20.0 ],
+					"id" : "obj-6",
+					"fontname" : "Arial",
+					"numinlets" : 1,
+					"numoutlets" : 2
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "multislider",
+					"contdata" : 1,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 40.0, 72.0, 20.0, 140.0 ],
+					"setminmax" : [ 0.0, 127.0 ],
+					"id" : "obj-1",
+					"numinlets" : 1,
+					"numoutlets" : 2
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "multislider",
+					"contdata" : 1,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 424.0, 68.0, 20.0, 140.0 ],
+					"setminmax" : [ 1.0, 10.0 ],
+					"id" : "obj-4",
+					"numinlets" : 1,
+					"numoutlets" : 2
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "multislider",
+					"contdata" : 1,
+					"outlettype" : [ "", "" ],
+					"patching_rect" : [ 236.0, 68.0, 20.0, 140.0 ],
+					"setminmax" : [ 1.0, 10.0 ],
+					"id" : "obj-3",
+					"numinlets" : 1,
+					"numoutlets" : 2
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "bdist",
+					"fontsize" : 12.0,
+					"outlettype" : [ "" ],
+					"patching_rect" : [ 40.0, 228.0, 404.0, 142.0 ],
+					"color" : [ 1.0, 1.0, 1.0, 1.0 ],
+					"ymax" : 127.0,
+					"id" : "obj-2",
+					"fontname" : "Arial",
+					"numinlets" : 3,
+					"xmax" : 127.0,
+					"a" : 7.913043,
+					"textcolor" : [  ],
+					"numoutlets" : 1
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"source" : [ "obj-1", 0 ],
+					"destination" : [ "obj-5", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-4", 0 ],
+					"destination" : [ "obj-2", 2 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-3", 0 ],
+					"destination" : [ "obj-2", 1 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-1", 0 ],
+					"destination" : [ "obj-2", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-2", 0 ],
+					"destination" : [ "obj-6", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-11", 0 ],
+					"destination" : [ "obj-3", 0 ],
+					"hidden" : 1,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-12", 0 ],
+					"destination" : [ "obj-3", 0 ],
+					"hidden" : 1,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-14", 0 ],
+					"destination" : [ "obj-4", 0 ],
+					"hidden" : 1,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-13", 0 ],
+					"destination" : [ "obj-4", 0 ],
+					"hidden" : 1,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-4", 0 ],
+					"destination" : [ "obj-24", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-3", 0 ],
+					"destination" : [ "obj-23", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+ ]
+	}
+
+}
