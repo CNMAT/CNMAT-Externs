365ad566531a8683e2405180aba004337e225afc
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2009-12-28 23:05:12 +0000

    added cmmjl_osc_pattern.[ch].  these replace the old files that have now been moved to cmmjl_osc_pattern_re.[ch].

---
+++ cmmjl/src/cmmjl_osc_pattern.h
@@ -0,0 +1,6 @@
+const char *cmmjl_osc_pattern_match(const char *pattern, const char *address);
+int cmmjl_osc_pattern_match_star(const char *pattern, const char *address);
+int cmmjl_osc_pattern_match_star_r(const char *pattern, const char *address);
+int cmmjl_osc_pattern_match_single_char(const char *pattern, const char *address);
+int cmmjl_osc_pattern_match_bracket(const char *pattern, const char *address);
+int cmmjl_osc_pattern_match_curly_brace(const char *pattern, const char *address);
