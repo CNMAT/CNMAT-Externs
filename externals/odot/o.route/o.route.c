811845e7a41f394ffc12688f18ba3a5870fa42f2
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2010-05-16 09:39:17 +0000

    rewrote o.route to use some new osc parsing code that is being developed for liboio

--- externals/odot/o.route/o.route.c
+++ externals/odot/o.route/o.route.c
@@ -35,18 +35,33 @@ VERSION 0.0: First try
 #include "version.c"
 #include "ext_obex.h"
 #include "ext_obex_util.h"
-#include "cmmjl/cmmjl.h"
-#include "cmmjl/cmmjl_osc.h"
-//#include "cmmjl/cmmjl_osc_obj.h"
+#include "omax_util.h"
+#include "osc_match.h"
+
+#define MAX_NUM_MESSAGES 1024
+
+typedef struct _oroute_message{
+	t_osc_msg msg;
+	int outlet_num;
+	int full_match;
+	int offset;
+	struct _oroute_message *next, *prev;
+} t_oroute_message;
 
 typedef struct _oroute{
 	t_object ob;
 	void **outlets;
 	t_symbol **args;
 	int numArgs;
-	t_cmmjl_osc_message *matched, *unmatched;
-	int *numCharsMatched;
-	int *matched_outlets;
+	// we have an array of preallocated messages that we'll use, but we'll
+	// organize them in a linked list as we accumulate them so that we can 
+	// just rip through that list when it's time to output them later
+	t_oroute_message *messages;
+	t_oroute_message *message_buf;
+	int numMessages, message_buf_len;
+	//t_cmmjl_osc_message *matched, *unmatched;
+	//int *numCharsMatched;
+	//int *matched_outlets;
 	int numMatched, numUnmatched;
 	int bundlePartialMatches;
 } t_oroute;
@@ -54,114 +69,146 @@ typedef struct _oroute{
 void *oroute_class;
 
 void oroute_fullPacket(t_oroute *x, long len, long ptr);
-void oroute_doFullPacket(t_oroute *x, long len, long ptr, int shouldOutputBundle);
-void oroute_foo(t_cmmjl_osc_message msg, void *v);
-void oroute_cbk(void *xx, t_symbol *msg, int argc, t_atom *argv);
+void oroute_fp_bundlePartialMatches(t_oroute *x, long len, char *ptr);
+void oroute_fp(t_oroute *x, long len, char *ptr);
+void oroute_cbk(t_osc_msg msg, void *context);
+void oroute_insert_msg(t_oroute *x, t_oroute_message *msg);
 void oroute_anything(t_oroute *x, t_symbol *msg, short argc, t_atom *argv);
 void oroute_free(t_oroute *x);
 void oroute_assist(t_oroute *x, void *b, long m, long a, char *s);
 void *oroute_new(t_symbol *msg, short argc, t_atom *argv);
 
-void oroute_fullPacket(t_oroute *x, long len, long ptr){
-	oroute_doFullPacket(x, len, ptr, 1);
-}
 
-void oroute_doFullPacket(t_oroute *x, long len, long ptr, int shouldOutputBundle){
-	// make a local copy so the ref doesn't disappear out from underneath us
+void oroute_fullPacket(t_oroute *x, long len, long ptr){
+	/*
+	int i;
+	for(i = 0; i < len; i++){
+		printf("%d: %c (0x%x)\n", i, ((char *)ptr)[i], ((char *)ptr)[i]);
+	}
+	*/
 	char cpy[len];
 	memcpy(cpy, (char *)ptr, len);
 	long nn = len;
-	//int k;
-
-	// if the OSC packet contains a single message, turn it into a bundle
 	if(strncmp(cpy, "#bundle\0", 8)){
-		/*
-		post("making bundle from naked message");
-		for(k = 0; k < nn; k++){
-			post("bundled: %d %c 0x%x", k, cpy[k], cpy[k]);
-		}
-		*/
-		nn = cmmjl_osc_bundle_naked_message(len, cpy, cpy);
+		nn = osc_util_bundle_naked_message(len, cpy, cpy);
 		if(nn < 0){
 			error("problem bundling naked message");
 		}
 	}
-
-	/*
-	for(k = 0; k < nn; k++){
-		post("bundled: %d %c 0x%x", k, cpy[k], cpy[k]);
-	}
-	*/
-	// flatten any nested bundles
-	nn = cmmjl_osc_flatten(nn, cpy, cpy);
-
-	// init the number of matched and unmatched items and extract the messages from the bundle
-	x->numMatched = x->numUnmatched = 0;
-	cmmjl_osc_extract_messages(nn, cpy, true, oroute_foo, (void *)x);
-
-	int i, j = 0;
-
-	// pack all unmatched messages into a new bundle and output the pointer to it out the right outlet
-	if(x->numUnmatched > 0){
-		char out[len];
-		j += cmmjl_osc_init_bundle(len, out, cpy + 8);
-		for(i = 0; i < x->numUnmatched; i++){
-			j += cmmjl_osc_add_to_bundle(len - j, out + j, &(x->unmatched[i]));
-		}
-
-		t_atom out_atoms[2];
-		atom_setlong(&(out_atoms[0]), j);
-		atom_setlong(&(out_atoms[1]), (long)out);
-		outlet_anything(x->outlets[x->numArgs], gensym("FullPacket"), 2, out_atoms);
-	}
-
-	// output all of the matches in right to left outlet order
-	for(j = x->numArgs - 1; j >= 0; j--){
-		for(i = 0; i < x->numMatched; i++){
-			//atom_setsym(&(out_atoms[0]), gensym(x->matched[i].address));
-			if(x->matched_outlets[i] == j){
-				t_atom out_atoms[x->matched[i].argc];
-				//cmmjl_osc_args2atoms(x->matched[i].typetags, x->matched[i].argv, out_atoms);
-				cmmjl_osc_get_data_atoms(x->matched + i, out_atoms);
-
-				t_symbol *sym = _sym_list;
-				if(x->numCharsMatched[i] > 0){
-					// this is a partial match
-					//post("partial match");
-					sym = gensym(x->matched[i].address + x->numCharsMatched[i]);
-					if(x->bundlePartialMatches){
-						//post("bundling partial match");
-						//int len = cmmjl_osc_get_msg_length(sym->s_name, x->matched[i].typetags, x->matched[i].argc, x->matched[i].argv);
-						int len = x->matched[i].size;
-						char buf[len];
-						memcpy(buf, x->matched[i].address + x->numCharsMatched[i], len);
-						t_atom out[2];
-						atom_setlong(out, len);
-						atom_setlong(out + 1, (long)buf);
-						outlet_anything(x->outlets[x->matched_outlets[i]], ps_FullPacket, 2, out);
-					}else{
-						outlet_anything(x->outlets[x->matched_outlets[i]], sym, x->matched[i].argc, out_atoms);
-					}
-				}else{
-					// this is a full match
-					if(x->matched[i].argc > 1){
-						outlet_anything(x->outlets[x->matched_outlets[i]], sym, x->matched[i].argc, out_atoms);
-					}else{
-						outlet_atoms(x->outlets[x->matched_outlets[i]], x->matched[i].argc, out_atoms);
-					}
-				}
-
-				if(x->matched[i].argc > 1){
-
-				}else{
-
-				}
-			}
-		}
+	nn = osc_util_flatten(nn, cpy, cpy);
+	x->messages = NULL;
+	x->numMessages = 0;
+	osc_util_parseBundleWithCallback(nn, cpy, oroute_cbk, (void *)x);
+	if(x->bundlePartialMatches){
+		oroute_fp_bundlePartialMatches(x, nn, cpy);
+	}else{
+		oroute_fp(x, nn, cpy);
 	}
 }
 
-void oroute_foo(t_cmmjl_osc_message msg, void *v){
+
+void oroute_fp_bundlePartialMatches(t_oroute *x, long len, char *ptr){
+	t_oroute_message *m = x->messages;
+	t_atomarray *a;
+	t_atom *argv;
+	long argc;
+	char buf[len], *bufp = buf;
+	memset(buf, '\0', len);
+	memcpy(buf, ptr, 16);
+	bufp += 16;
+	int last_outlet_num = -1;
+	while(m){
+		if(last_outlet_num != m->outlet_num && last_outlet_num >= 0 && bufp - buf > 16){
+			t_atom out[2];
+			atom_setlong(out, bufp - buf);
+			atom_setlong(out + 1, (long)buf);
+			outlet_anything(x->outlets[m->outlet_num], gensym("FullPacket"), 2, out);
+			bufp = buf + 16;
+			memset(bufp, '\0', len - 16);
+		}
+		if(m->full_match){
+			a = omax_util_oscMsg2MaxAtoms(&(m->msg));
+			atomarray_getatoms(a, &argc, &argv);
+			outlet_list(x->outlets[m->outlet_num], NULL, argc - 1, argv + 1);
+			atomarray_clear(a);
+		}else{
+			char *size = bufp;
+			bufp += 4;
+			strcpy(bufp, m->msg.address + m->offset);
+			bufp += strlen(m->msg.address + m->offset);
+			bufp++;
+			while((bufp - buf) % 4){
+				bufp++;
+			}
+			memcpy(bufp, m->msg.typetags, m->msg.size - (m->msg.typetags - m->msg.address));
+			bufp += m->msg.size - (m->msg.typetags - m->msg.address);
+			*((uint32_t *)size) = hton32(bufp - size - 4);
+		}
+		last_outlet_num = m->outlet_num;
+		m = m->next;
+	}
+	if(bufp - buf > 16){
+		t_atom out[2];
+		atom_setlong(out, bufp - buf);
+		atom_setlong(out + 1, (long)buf);
+		outlet_anything(x->outlets[last_outlet_num], gensym("FullPacket"), 2, out);
+	}
+}
+
+void oroute_fp(t_oroute *x, long len, char *ptr){
+	t_oroute_message *m = x->messages;
+	t_atomarray *a = NULL;
+	t_atom *argv;
+	long argc;
+	while(m){
+	        a = omax_util_oscMsg2MaxAtoms(&(m->msg));
+		if(m->full_match){
+			atomarray_getatoms(a, &argc, &argv);
+			outlet_list(x->outlets[m->outlet_num], NULL, argc - 1, argv + 1);
+			atomarray_clear(a);
+		}else{
+			atomarray_getatoms(a, &argc, &argv);
+			if(argc){
+				outlet_anything(x->outlets[m->outlet_num], gensym(((atom_getsym(argv)->s_name) + m->offset)), argc - 1, argv + 1);
+				atomarray_clear(a);
+			}
+		}
+		m = m->next;
+	}
+}
+
+void oroute_cbk(t_osc_msg msg, void *context){
+	if(!(msg.address)){
+		return;
+	}
+	t_oroute *x = (t_oroute *)context;
+	int i;
+	int match = 0;
+	for(i = 0; i < x->numArgs; i++){
+		int po, ao;
+		int ret = osc_match(msg.address, x->args[i]->s_name, &po, &ao);
+		if((ret & OSC_MATCH_ADDRESS_COMPLETE) && ((ret & OSC_MATCH_PATTERN_COMPLETE) || (msg.address[po] == '/'))){
+			x->message_buf[x->numMessages].msg = msg;
+			x->message_buf[x->numMessages].full_match = 0;
+			x->message_buf[x->numMessages].offset = po;
+			if(ret == 3){
+				x->message_buf[x->numMessages].full_match = 1;
+			}
+			x->message_buf[x->numMessages].outlet_num = i;
+			oroute_insert_msg(x, &(x->message_buf[x->numMessages]));
+			match = 1;
+			x->numMessages++;
+		}
+	}
+	if(!match){
+		x->message_buf[x->numMessages].msg = msg;
+		x->message_buf[x->numMessages].full_match = 0;
+		x->message_buf[x->numMessages].offset = 0;
+		x->message_buf[x->numMessages].outlet_num = x->numArgs;
+		oroute_insert_msg(x, &(x->message_buf[x->numMessages]));
+		x->numMessages++;
+	}
+	/*
 	t_oroute *x = (t_oroute *)v;
 	int i;
 	int ret;
@@ -184,9 +231,44 @@ void oroute_foo(t_cmmjl_osc_message msg, void *v){
 	if(didmatch == 0){
 		x->unmatched[x->numUnmatched++] = msg;
 	}
+	*/
+}
+
+void oroute_insert_msg(t_oroute *x, t_oroute_message *message){
+	t_oroute_message *msg = message;
+	t_oroute_message *m = x->messages;
+	t_oroute_message *prev = NULL;
+	msg->next = NULL;
+	msg->prev = NULL;
+	if(!m){
+		x->messages = msg;
+		return;
+	}
+	while(m){
+		if(m->outlet_num <= msg->outlet_num){
+			break;
+		}
+		prev = m;
+		m = m->next;
+	}
+	if(!m){
+		m = msg;
+		msg = prev;
+	}
+	msg->next = m;
+	msg->prev = NULL;
+	if(m){
+		msg->prev = m->prev;
+		if(!(m->prev)){
+			x->messages = msg;
+		}
+		m->prev = msg;
+	}
+	//post("%p <-- %p --> %p", msg->prev, msg, msg->next);
 }
 
 void oroute_anything(t_oroute *x, t_symbol *msg, short argc, t_atom *argv){
+	/*
 	int len = cmmjl_osc_get_msg_length_max(msg, argc, argv);
 	if(len == 0){
 		error("o.route: problem formatting bundle");
@@ -197,6 +279,7 @@ void oroute_anything(t_oroute *x, t_symbol *msg, short argc, t_atom *argv){
 	cmmjl_osc_copy_max_messages(msg, argc, argv, len, buf);
 
 	oroute_doFullPacket(x, len, (long)buf, 0);
+	*/
 }
 
 void oroute_assist(t_oroute *x, void *b, long m, long a, char *s){
@@ -221,23 +304,25 @@ void *oroute_new(t_symbol *msg, short argc, t_atom *argv){
 	t_oroute *x;
 	int i;
 	if(x = (t_oroute *)object_alloc(oroute_class)){
-		cmmjl_init(x, NAME, 0);
+		//cmmjl_init(x, NAME, 0);
 		int numArgs = attr_args_offset(argc, argv);
 		x->outlets = (void **)malloc((numArgs + 1) * sizeof(void *));
 		x->args = (t_symbol **)malloc(numArgs * sizeof(t_symbol *));
 		x->numArgs = numArgs;
+		x->message_buf = (t_oroute_message *)sysmem_newptr(MAX_NUM_MESSAGES * sizeof(t_oroute_message));
+		x->messages = NULL;
 		x->outlets[numArgs] = outlet_new(x, "FullPacket"); // unmatched outlet
 		for(i = 0; i < numArgs; i++){
 			x->outlets[numArgs - 1 - i] = outlet_new(x, NULL);
 			x->args[i] = atom_getsym(argv + i);
-			x->matched = (t_cmmjl_osc_message *)malloc(1024 * sizeof(t_cmmjl_osc_message));
-			x->numCharsMatched = (int *)malloc(1024 * sizeof(int));
-			x->unmatched = (t_cmmjl_osc_message *)malloc(1024 * sizeof(t_cmmjl_osc_message));
-			x->matched_outlets = (int *)malloc(1024 * sizeof(int));
-			x->numMatched = 0;
-			x->numUnmatched = 0;
+			//x->matched = (t_cmmjl_osc_message *)malloc(1024 * sizeof(t_cmmjl_osc_message));
+			//x->numCharsMatched = (int *)malloc(1024 * sizeof(int));
+			//x->unmatched = (t_cmmjl_osc_message *)malloc(1024 * sizeof(t_cmmjl_osc_message));
+			//x->matched_outlets = (int *)malloc(1024 * sizeof(int));
+			//x->numMatched = 0;
+			//x->numUnmatched = 0;
 		}
-		x->bundlePartialMatches = 0;
+		x->bundlePartialMatches = 1;
 		attr_args_process(x, argc, argv);
 	}
 		   	
