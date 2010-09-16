d195ed450d769ba3362a34350ede6236ab4cab46
Yotam Mann <Yotam Mann@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2010-09-16 20:31:57 +0000

    created a max patch for compressing and tranmitting jitter matrices over a network

---
+++ mspexternals/mixing/cnmatrix~/version.h
@@ -0,0 +1,16 @@
+#ifndef NAME
+#define NAME "jit.rtp.send"
+#endif
+#ifndef DESCRIPTION
+#define DESCRIPTION "similar to matrix~, but takes jitter matricies"
+#endif
+#ifndef AUTHORS
+#define AUTHORS "Yotam Mann"
+#endif
+#ifndef COPYRIGHT_YEARS
+#define COPYRIGHT_YEARS "2009, 1999"
+#endif
+#ifndef SVN_REVISION
+#define SVN_REVISION "$LastChangedRevision: 587 $"
+#endif
+#error "No VERSION defined!"
