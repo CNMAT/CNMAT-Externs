6cb9840afccedf7fcf06f11695aee80b409c5d16
Jeff Lubow <Jeff Lubow@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2011-01-15 22:31:45 +0000

    checked in o.print

---
+++ externals/odot/o.print/o.print.c
@@ -0,0 +1,171 @@
+/*
+Written by John MacCallum, The Center for New Music and Audio Technologies,
+University of California, Berkeley.  Copyright (c) 2011, The Regents of
+the University of California (Regents). 
+Permission to use, copy, modify, distribute, and distribute modified versions
+of this software and its documentation without fee and without a signed
+licensing agreement, is hereby granted, provided that the above copyright
+notice, this paragraph and the following two paragraphs appear in all copies,
+modifications, and distributions.
+
+IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS
+BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
+MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+NAME: o.print
+DESCRIPTION: Print the contents of an OSC bundle to the Max window
+AUTHORS: John MacCallum
+COPYRIGHT_YEARS: 2011
+SVN_REVISION: $LastChangedRevision: 587 $
+VERSION 0.0: First try
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
+
+#include "version.h"
+#include "ext.h"
+#include "version.c"
+#include "ext_obex.h"
+#include "ext_obex_util.h"
+#include "omax_util.h"
+#include "liboio/osc_util.h"
+#include "liboio/osc_match.h"
+
+typedef struct _oprint{
+	t_object ob;
+	int print_msgsize;
+	int print_typetags;
+} t_oprint;
+
+void *oprint_class;
+
+void oprint_fullPacket(t_oprint *x, long len, long ptr);
+void oprint_cbk(t_osc_msg msg, void *v);
+void oprint_int(t_oprint *x, long l);
+void oprint_float(t_oprint *x, double f);
+void oprint_anything(t_oprint *x, t_symbol *msg, short argc, t_atom *argv);
+void oprint_list(t_oprint *x, t_symbol *msg, short argc, t_atom *argv);
+void oprint_free(t_oprint *x);
+void oprint_assist(t_oprint *x, void *b, long m, long a, char *s);
+void *oprint_new(t_symbol *msg, short argc, t_atom *argv);
+t_max_err oprint_notify(t_oprint *x, t_symbol *s, t_symbol *msg, void *sender, void *data);
+
+t_symbol *ps_FullPacket;
+
+void oprint_fullPacket(t_oprint *x, long len, long ptr){
+	// make a local copy so the ref doesn't disappear out from underneath us
+	char cpy[len];
+	memcpy(cpy, (char *)ptr, len);
+	long nn = len;
+
+	// if the OSC packet contains a single message, turn it into a bundle
+	if(strncmp(cpy, "#bundle\0", 8)){
+		nn = osc_util_bundle_naked_message(len, cpy, cpy);
+		if(nn < 0){
+			error("problem bundling naked message");
+		}
+	}
+
+	// flatten any nested bundles
+	nn = osc_util_flatten(nn, cpy, cpy);
+
+	// extract the messages from the bundle
+	post("[ 0x%x", ntoh64(*((uint64 *)(cpy + 8))));
+	osc_util_parseBundleWithCallback(nn, cpy, oprint_cbk, (void *)x);
+	post("]");
+}
+
+void oprint_cbk(t_osc_msg msg, void *v){
+	t_oprint *x = (t_oprint *)v;
+	long len = msg.argc;
+	t_atom atoms[len + 1];
+	omax_util_oscMsg2MaxAtoms(&msg, &len, atoms);
+	char buf[256];
+	int bufpos = 0;
+	if(x->print_msgsize){
+		bufpos += sprintf(buf, "\t\t(%d) %s", msg.size, msg.address);
+	}else{
+		bufpos += sprintf(buf, "\t\t%s", msg.address);
+	}
+	if(x->print_typetags){
+		bufpos += sprintf(buf + bufpos, " %s", msg.typetags);
+	}
+	int i;
+	for(i = 1; i < len + 1; i++){
+		switch(atom_gettype(atoms + i)){
+		case A_LONG:
+			bufpos += sprintf(buf + bufpos, " %ld", atom_getlong(atoms + i));
+			break;
+		case A_FLOAT:
+			bufpos += sprintf(buf + bufpos, " %f", atom_getfloat(atoms + i));
+			break;
+		case A_SYM:
+			bufpos += sprintf(buf + bufpos, " %s", atom_getsym(atoms + i)->s_name);
+			break;
+		}
+	}
+	post("%s", buf);
+}
+
+void oprint_assist(t_oprint *x, void *b, long m, long a, char *s){
+	if (m == ASSIST_OUTLET)
+		sprintf(s,"Probability distribution and arguments");
+	else {
+		switch (a) {	
+		case 0:
+			sprintf(s,"Random variate");
+			break;
+		}
+	}
+}
+
+void oprint_free(t_oprint *x){
+}
+
+void *oprint_new(t_symbol *msg, short argc, t_atom *argv){
+	t_oprint *x;
+	if(x = (t_oprint *)object_alloc(oprint_class)){
+		attr_args_process(x, argc, argv);
+	}
+		   	
+	return(x);
+}
+
+int main(void){
+	t_class *c = class_new("o.print", (method)oprint_new, (method)oprint_free, sizeof(t_oprint), 0L, A_GIMME, 0);
+    
+	class_addmethod(c, (method)oprint_fullPacket, "FullPacket", A_LONG, A_LONG, 0);
+	//class_addmethod(c, (method)oprint_notify, "notify", A_CANT, 0);
+	class_addmethod(c, (method)oprint_assist, "assist", A_CANT, 0);
+	class_addmethod(c, (method)oprint_notify, "notify", A_CANT, 0);
+
+	CLASS_ATTR_LONG(c, "printtypetags", 0, t_oprint, print_typetags);
+	CLASS_ATTR_LONG(c, "printsize", 0, t_oprint, print_msgsize);
+
+	class_register(CLASS_BOX, c);
+	oprint_class = c;
+
+	ps_FullPacket = gensym("FullPacket");
+
+	common_symbols_init();
+	return 0;
+}
+
+t_max_err oprint_notify(t_oprint *x, t_symbol *s, t_symbol *msg, void *sender, void *data){
+	/*
+	t_symbol *attrname;
+        if(msg == gensym("attr_modified")){
+		attrname = (t_symbol *)object_method((t_object *)data, gensym("getname"));
+		}
+	}
+	*/
+	return MAX_ERR_NONE;
+}
---
+++ externals/odot/o.print/o.print.maxhelp
@@ -0,0 +1,196 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"rect" : [ 521.0, 44.0, 1034.0, 418.0 ],
+		"bglocked" : 0,
+		"defrect" : [ 521.0, 44.0, 1034.0, 418.0 ],
+		"openrect" : [ 0.0, 0.0, 0.0, 0.0 ],
+		"openinpresentation" : 0,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 0,
+		"gridsize" : [ 10.0, 10.0 ],
+		"gridsnaponopen" : 0,
+		"toolbarvisible" : 1,
+		"boxanimatetime" : 200,
+		"imprint" : 0,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"boxes" : [ 			{
+				"box" : 				{
+					"maxclass" : "o.message",
+					"text" : "/foo 1 2.0 bar",
+					"numoutlets" : 1,
+					"patching_rect" : [ 330.0, 40.0, 79.0, 20.0 ],
+					"outlettype" : [ "" ],
+					"id" : "obj-6",
+					"fontname" : "Arial",
+					"textcolor" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"fontsize" : 12.0,
+					"numinlets" : 2
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "newobj",
+					"text" : "o.print @printtypetags 1 @printsize 1",
+					"numoutlets" : 0,
+					"patching_rect" : [ 330.0, 70.0, 208.0, 20.0 ],
+					"id" : "obj-7",
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"numinlets" : 1
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "o.message",
+					"text" : "/foo 1 2.0 bar",
+					"numoutlets" : 1,
+					"patching_rect" : [ 180.0, 40.0, 79.0, 20.0 ],
+					"outlettype" : [ "" ],
+					"id" : "obj-3",
+					"fontname" : "Arial",
+					"textcolor" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"fontsize" : 12.0,
+					"numinlets" : 2
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "newobj",
+					"text" : "o.print @printtypetags 1",
+					"numoutlets" : 0,
+					"patching_rect" : [ 180.0, 70.0, 138.0, 20.0 ],
+					"id" : "obj-5",
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"numinlets" : 1
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "o.message",
+					"text" : "/foo 1 2.0 bar",
+					"numoutlets" : 1,
+					"patching_rect" : [ 80.0, 40.0, 79.0, 20.0 ],
+					"outlettype" : [ "" ],
+					"id" : "obj-2",
+					"fontname" : "Arial",
+					"textcolor" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"fontsize" : 12.0,
+					"numinlets" : 2
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "newobj",
+					"text" : "o.print",
+					"numoutlets" : 0,
+					"patching_rect" : [ 80.0, 70.0, 44.0, 20.0 ],
+					"id" : "obj-1",
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"numinlets" : 1
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "textbutton",
+					"numoutlets" : 3,
+					"border" : 0,
+					"bgovercolor" : [ 0.698039, 0.698039, 0.698039, 0.0 ],
+					"text" : "http://cnmat.berkeley.edu/downloads",
+					"patching_rect" : [ 50.0, 350.0, 223.0, 21.0 ],
+					"outlettype" : [ "", "", "int" ],
+					"bgcolor" : [ 0.74902, 0.74902, 0.74902, 0.0 ],
+					"bgoveroncolor" : [ 0.501961, 0.501961, 0.501961, 0.0 ],
+					"id" : "obj-4",
+					"fontname" : "Arial",
+					"textcolor" : [ 0.0, 0.0, 1.0, 1.0 ],
+					"bgoncolor" : [ 0.54902, 0.54902, 0.54902, 0.0 ],
+					"textoncolor" : [ 0.0, 0.0, 1.0, 1.0 ],
+					"fontsize" : 12.0,
+					"numinlets" : 1,
+					"textovercolor" : [ 0.0, 0.0, 1.0, 1.0 ],
+					"textoveroncolor" : [ 0.0, 0.0, 1.0, 1.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "message",
+					"text" : ";\rmax launchbrowser http://cnmat.berkeley.edu/downloads",
+					"linecount" : 2,
+					"numoutlets" : 1,
+					"patching_rect" : [ 60.0, 360.0, 312.0, 32.0 ],
+					"outlettype" : [ "" ],
+					"id" : "obj-15",
+					"fontname" : "Arial",
+					"fontsize" : 12.0,
+					"hidden" : 1,
+					"numinlets" : 2
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "fpic",
+					"numoutlets" : 0,
+					"patching_rect" : [ 60.0, 300.0, 109.0, 48.0 ],
+					"id" : "obj-36",
+					"numinlets" : 1,
+					"embed" : 1,
+					"data" : [ 593, "", "IBkSG0fBZn....PCIgDQRA...zF....LHX....vFnS+e....DLmPIQEBHf.B7g.YHB..BfPRDEDU3wY6bEsiCCBCKLc+++xbOwItcqkVHN1oGVZZRSCWZbLgNfUp0Z01HU3K1cfMLqTJC+N8dqOJZ2kjQs0Cy7Sk2qDqaeuV6esBIWssWky6bs9Oy6qO8gH.RtmAn5O2k2Yb0SWSalKVuE2KffyYwrIBm0++DmmJZpDLx.NpFFBG8epo4IVovKR7deHaImPEMTPsfLpYfdDfKZQ31Tv8FIBwoolyvSDsKyLhCOxxcftdVDIngIZnCNLRBXk3QchHOsZQQUFHTQSsZaqzeXlvQeJ+Qdyi7ZEYBY3hlm2brpqwdXc5NMy3GDVEQOrOEQSgZay1GTHASBmlY3CFn3mQBHMQyqa1HqqofKyLxNMEd.4U.qg4kY3wHwrKfqJftn4gaajHfHfybxTzEMD3ouzORHZYq1F6GYQBQKRb2.thaMAYDsUcaG0d0csy.YDMEghtLyDSzxVsMVPJQyLeEtUbJp5xLavlUMaY5n1bndBO5e+xogbsthpsHfJ2KGdpY73hfjiQbGc6tB7lix9jflOH2DQ1XL1hVBwVzRH1hVBwVzRH1hVBws+KoX0mPXzuH.5spcV4um2ScZ0Z8mWdidNQehZP0+QFaNi6gCOVJkTtmAabiLgqEWP7O1P+6uC5G0ITBWi6mxgtnGCEMTCAzPV2bosQfXrpBzVZldtQu6oxF+i3b+CFmP7MQ+TMlEbQ3RG.....IUjSD4pPfIH" ]
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"source" : [ "obj-6", 0 ],
+					"destination" : [ "obj-7", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-3", 0 ],
+					"destination" : [ "obj-5", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-4", 0 ],
+					"destination" : [ "obj-15", 0 ],
+					"hidden" : 1,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-2", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+ ]
+	}
+
+}
