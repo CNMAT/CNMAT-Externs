a58c9d8c7a1982360dd0568da2b194a33ee1c0cb
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2011-03-30 21:09:34 +0000

    bench now accepts lists

--- externals/benchmarking/bench/bench.c
+++ externals/benchmarking/bench/bench.c
@@ -32,10 +32,14 @@ VERSION 1.0: First version
 
 #include "version.h"
 #include "ext.h"
+#include "ext_obex.h"
 #include "version.c"
 #include "math.h"
-#include "sys/time.h"
-#include "cycle.h"
+//#include "sys/time.h"
+//#include "cycle.h"
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+#include <unistd.h>
 
 #define BENCH_IN 0
 #define BENCH_OUT 1
@@ -47,30 +51,35 @@ typedef struct _bench{
 	void *out0;
 	void *out1;
 	int t_objmode;
-	unsigned long long time;
+	uint64_t time;
 } t_bench;
 
-void *bench_class;
+t_class *bench_class;
 
 void bench_assist(t_bench *x, void *b, long m, long a, char *s);
 void *bench_new(t_symbol *mode);
 void bench_free(t_bench *x);
 void bench_anything(t_bench *x, t_symbol *msg, short argc, t_atom *argv);
+void bench_list(t_bench *x, t_symbol *msg, short argc, t_atom *argv);
 void bench_int(t_bench *x, long i);
 void bench_float(t_bench *x, double f);
 void bench_bang(t_bench *x);
 
 int main(void){
-	setup((t_messlist **)&bench_class, (method)bench_new, (method)bench_free, (short)sizeof(t_bench), 0L, A_SYM, 0);
+	t_class *c = class_new("bench", (method)bench_new, (method)bench_free, (short)sizeof(t_bench), 0L, A_SYM, 0);
 	
 	version(0);
 
-	addmess((method) version, "version", 0);
-	addmess((method)bench_assist, "assist", A_CANT, 0);
-	addmess((method)bench_anything, "anything", A_GIMME, 0);
-	addint((method)bench_int);
-	addfloat((method)bench_float);
-        addbang((method)bench_bang);
+	class_addmethod(c, (method) version, "version", 0);
+	class_addmethod(c, (method)bench_assist, "assist", A_CANT, 0);
+	class_addmethod(c, (method)bench_anything, "anything", A_GIMME, 0);
+	class_addmethod(c, (method)bench_list, "list", A_GIMME, 0);
+	class_addmethod(c, (method)bench_int, "int", 0);
+	class_addmethod(c, (method)bench_float, "float", 0);
+	class_addmethod(c, (method)bench_bang, "bang", 0);
+
+	bench_class = c;
+	class_register(CLASS_BOX, c);
 	
 	return 0;
 }
@@ -85,7 +94,10 @@ void bench_assist(t_bench *x, void *b, long m, long a, char *s){
 void *bench_new(t_symbol *mode){
 	t_bench *x;
 
-	x = (t_bench *)newobject(bench_class);
+	x = (t_bench *)object_alloc(bench_class);
+	if(!x){
+		return NULL;
+	}
 
 	if(!strcmp(mode->s_name, "in")){
 		x->out1 = listout((t_object *)x);
@@ -109,14 +121,14 @@ void bench_free(t_bench *x){
 void bench_anything(t_bench *x, t_symbol *msg, short argc, t_atom *argv){
 	t_atom out[2];
 	if(x->t_objmode == BENCH_IN){
-		ticks t = getticks();
-		unsigned long l1, l2;
+		uint64_t t = mach_absolute_time();
+		uint32_t l1, l2;
 
-		l1 = (unsigned long)((t & 0xffffffff00000000LL) >> 32);
-		l2 = (unsigned long)(t & 0xffffffffLL);
+		l1 = (uint32_t)((t & 0xffffffff00000000LL) >> 32);
+		l2 = (uint32_t)(t & 0xffffffffLL);
 		SETLONG(&out[0], l1);
 		SETLONG(&out[1], l2);
-		outlet_anything(x->out1, gensym("list"), 2, out);
+		outlet_list(x->out1, NULL, 2, out);
 		outlet_anything(x->out0, msg, argc, argv);
 	}else{
 		if(proxy_getinlet((t_object *)x) == 1){
@@ -124,12 +136,41 @@ void bench_anything(t_bench *x, t_symbol *msg, short argc, t_atom *argv){
 				error("bench: the right inlet should get 2 numbers, only got %d", argc);
 				return;
 			}
-			long l1 = argv[0].a_w.w_long;
-			long l2 = argv[1].a_w.w_long;
-			unsigned long long t1;
-			t1 = *((unsigned long *)(&l1));
+			int32_t l1 = argv[0].a_w.w_long;
+			int32_t l2 = argv[1].a_w.w_long;
+			uint64_t t1;
+			t1 = *((uint32_t *)(&l1));
 			t1 = t1 << 32;
-			t1 = t1 | *((unsigned long *)(&l2));
+			t1 = t1 | *((uint32_t *)(&l2));
+			x->time = t1;
+		}
+	}
+}
+
+void bench_list(t_bench *x, t_symbol *msg, short argc, t_atom *argv){
+	t_atom out[2];
+	if(x->t_objmode == BENCH_IN){
+		uint64_t t = mach_absolute_time();
+		uint32_t l1, l2;
+
+		l1 = (uint32_t)((t & 0xffffffff00000000LL) >> 32);
+		l2 = (uint32_t)(t & 0xffffffffLL);
+		SETLONG(&out[0], l1);
+		SETLONG(&out[1], l2);
+		outlet_list(x->out1, NULL, 2, out);
+		outlet_list(x->out0, msg, argc, argv);
+	}else{
+		if(proxy_getinlet((t_object *)x) == 1){
+			if(argc != 2){
+				error("bench: the right inlet should get 2 numbers, only got %d", argc);
+				return;
+			}
+			int32_t l1 = argv[0].a_w.w_long;
+			int32_t l2 = argv[1].a_w.w_long;
+			uint64_t t1;
+			t1 = *((uint32_t *)(&l1));
+			t1 = t1 << 32;
+			t1 = t1 | *((uint32_t *)(&l2));
 			x->time = t1;
 		}
 	}
@@ -137,40 +178,52 @@ void bench_anything(t_bench *x, t_symbol *msg, short argc, t_atom *argv){
 
 void bench_bang(t_bench *x){
 	if(x->t_objmode == BENCH_OUT){
-		ticks t2 = getticks();
-		double diff = elapsed(t2, (ticks)x->time);
-		outlet_float(x->out0, diff);
+		uint64_t t2 = mach_absolute_time();
+		uint64_t diff = t2 - x->time;
+		UnsignedWide elapsedNano = AbsoluteToNanoseconds(*(AbsoluteTime *)&diff);
+		uint64_t en = UnsignedWideToUInt64(elapsedNano);
+		//post("%f, %f", (double)en, (double)en * 0.000001);
+		outlet_float(x->out0, (double)en * 0.000001);
+	}else{
+		t_atom out[2];
+		uint64_t t = mach_absolute_time();
+		uint32_t l1, l2;
+
+		l1 = (uint32_t)((t & 0xffffffff00000000LL) >> 32);
+		l2 = (uint32_t)(t & 0xffffffffLL);
+		atom_setlong(out, l1);
+		atom_setlong(out + 1, l2);
+		outlet_list(x->out1, NULL, 2, out);
+		outlet_bang(x->out0);
 	}
 }
 
 void bench_float(t_bench *x, double f){
 	t_atom out[2];
 	if(x->t_objmode == BENCH_IN){
-		ticks t = getticks();
-		unsigned long l1, l2;
+		uint64_t t = mach_absolute_time();
+		uint32_t l1, l2;
 
-		l1 = (unsigned long)((t & 0xffffffff00000000LL) >> 32);
-		l2 = (unsigned long)(t & 0xffffffffLL);
-		SETLONG(&out[0], l1);
-		SETLONG(&out[1], l2);
-		outlet_anything(x->out1, gensym("list"), 2, out);
-		SETFLOAT(&out[0], f);
-		outlet_anything(x->out0, gensym("float"), 1, out);
+		l1 = (uint32_t)((t & 0xffffffff00000000LL) >> 32);
+		l2 = (uint32_t)(t & 0xffffffffLL);
+		atom_setlong(out, l1);
+		atom_setlong(out + 1, l2);
+		outlet_list(x->out1, NULL, 2, out);
+		outlet_float(x->out0, f);
 	}
 }
 
 void bench_int(t_bench *x, long i){
 	t_atom out[2];
 	if(x->t_objmode == BENCH_IN){
-		ticks t = getticks();
-		unsigned long l1, l2;
+		uint64_t t = mach_absolute_time();
+		uint32_t l1, l2;
 
-		l1 = (unsigned long)((t & 0xffffffff00000000LL) >> 32);
-		l2 = (unsigned long)(t & 0xffffffffLL);
-		SETLONG(&out[0], l1);
-		SETLONG(&out[1], l2);
-		outlet_anything(x->out1, gensym("list"), 2, out);
-		SETFLOAT(&out[0], i);
-		outlet_anything(x->out0, gensym("long"), 1, out);
+		l1 = (uint32_t)((t & 0xffffffff00000000LL) >> 32);
+		l2 = (uint32_t)(t & 0xffffffffLL);
+		atom_setlong(out, l1);
+		atom_setlong(out + 1, l2);
+		outlet_list(x->out1, NULL, 2, out);
+		outlet_int(x->out0, i);
 	}
 }
