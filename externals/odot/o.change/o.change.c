8d2b102189d5c1885df38eeca375e584c6110ce7
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2011-07-25 19:51:34 +0000

    added o.change to the repository

---
+++ externals/odot/o.change/o.change.c
@@ -0,0 +1,183 @@
+/*
+  Written by John MacCallum, The Center for New Music and Audio Technologies,
+  University of California, Berkeley.  Copyright (c) 2011, The Regents of
+  the University of California (Regents). 
+  Permission to use, copy, modify, distribute, and distribute modified versions
+  of this software and its documentation without fee and without a signed
+  licensing agreement, is hereby granted, provided that the above copyright
+  notice, this paragraph and the following two paragraphs appear in all copies,
+  modifications, and distributions.
+
+  IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+  SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS, ARISING
+  OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF REGENTS HAS
+  BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+  REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+  PURPOSE. THE SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+  HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
+  MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+
+
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  NAME: o.change
+  DESCRIPTION: Pass a bundle through if it is different from the previous one
+  AUTHORS: John MacCallum
+  COPYRIGHT_YEARS: 2011
+  SVN_REVISION: $LastChangedRevision: 587 $
+  VERSION 0.0: First try
+  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
+
+#include "version.h"
+#include "ext.h"
+#include "version.c"
+#include "ext_obex.h"
+#include "ext_critical.h"
+#include "ext_obex_util.h"
+#include "osc.h"
+#include "omax_util.h"
+
+typedef struct _ochange{
+	t_object ob;
+	void *outlet;
+	int buflen, bufsize;
+	char *buf;
+	t_critical lock;
+} t_ochange;
+
+void *ochange_class;
+
+void ochange_fullPacket(t_ochange *x, long len, long ptr);
+void ochange_cbk(t_osc_msg msg, void *v);
+void ochange_output(void *outlet, long len, char *ptr);
+int ochange_copybundle(t_ochange *x, long len, char *ptr);
+void ochange_clear(t_ochange *x);
+void ochange_anything(t_ochange *x, t_symbol *msg, int argc, t_atom *argv);
+void ochange_bang(t_ochange *x);
+void ochange_free(t_ochange *x);
+void ochange_assist(t_ochange *x, void *b, long m, long a, char *s);
+void *ochange_new(t_symbol *msg, short argc, t_atom *argv);
+
+t_symbol *ps_FullPacket;
+
+void ochange_fullPacket(t_ochange *x, long len, long ptr){
+	if(!x->buf || x->buflen == 0){
+		ochange_copybundle(x, len, (char *)ptr);
+		ochange_output(x->outlet, len, (char *)ptr);
+		return;
+	}
+	long buflen = x->buflen;
+	char buf[buflen];
+	critical_enter(x->lock);
+	memcpy(buf, x->buf, buflen);
+	critical_exit(x->lock);
+	if(buflen == len){
+		if(!memcmp(buf, (char *)ptr, buflen)){
+			return;
+		}
+	}
+	ochange_copybundle(x, len, (char *)ptr);
+	ochange_output(x->outlet, len, (char *)ptr);
+}
+
+void ochange_output(void *outlet, long len, char *ptr){
+	t_atom out[2];
+	atom_setlong(out, len);
+	atom_setlong(out + 1, (long)ptr);
+	outlet_anything(outlet, ps_FullPacket, 2, out);
+}
+
+int ochange_copybundle(t_ochange *x, long len, char *ptr){
+	char *buf = (char *)osc_mem_alloc(len);
+	if(buf){
+		memcpy(buf, (char *)ptr, len);
+		critical_enter(x->lock);
+		char *oldbuf = x->buf;
+		x->buf = buf;
+		x->bufsize = x->buflen = len;
+		osc_mem_free(oldbuf);
+		critical_exit(x->lock);
+	}else{
+		object_error((t_object *)x, "out of memory!");
+		return 1;
+	}
+	return 0;
+}
+
+void ochange_clear(t_ochange *x){
+	x->buflen = 0;
+}
+
+void ochange_anything(t_ochange *x, t_symbol *msg, int argc, t_atom *argv){
+}
+
+void ochange_bang(t_ochange *x){
+	long len = x->buflen;
+	char buf[len];
+	critical_enter(x->lock);
+	memcpy(buf, x->buf, len);
+	critical_exit(x->lock);
+	ochange_output(x->outlet, len, buf);
+}
+
+void ochange_assist(t_ochange *x, void *b, long m, long a, char *s){
+	if (m == ASSIST_OUTLET)
+		sprintf(s, "OSC FullPacket");
+	else {
+		switch (a) {	
+		case 0:
+			sprintf(s, "The bundle if it has changed.");
+			break;
+		}
+	}
+}
+
+void ochange_free(t_ochange *x){
+	critical_free(x->lock);
+	if(x->buf){
+		osc_mem_free(x->buf);
+	}
+}
+
+void *ochange_new(t_symbol *msg, short argc, t_atom *argv){
+	t_ochange *x;
+	if(x = (t_ochange *)object_alloc(ochange_class)){
+		x->outlet = outlet_new((t_object *)x, "FullPacket");
+		critical_new(&(x->lock));
+		x->buf = NULL;
+		x->bufsize = x->buflen = 0;
+	}
+		   	
+	return(x);
+}
+
+int main(void){
+	t_class *c = class_new("o.change", (method)ochange_new, (method)ochange_free, sizeof(t_ochange), 0L, A_GIMME, 0);
+	class_addmethod(c, (method)ochange_fullPacket, "FullPacket", A_LONG, A_LONG, 0);
+	class_addmethod(c, (method)ochange_assist, "assist", A_CANT, 0);
+	//class_addmethod(c, (method)ochange_notify, "notify", A_CANT, 0);
+	class_addmethod(c, (method)ochange_bang, "bang", 0);
+	class_addmethod(c, (method)ochange_anything, "anything", A_GIMME, 0);
+	class_addmethod(c, (method)ochange_clear, "clear", 0);
+	
+	class_register(CLASS_BOX, c);
+	ochange_class = c;
+
+	common_symbols_init();
+	ps_FullPacket = gensym("FullPacket");
+
+	version(0);
+	return 0;
+}
+/*
+t_max_err ochange_notify(t_ochange *x, t_symbol *s, t_symbol *msg, void *sender, void *data){
+	t_symbol *attrname;
+
+        if(msg == gensym("attr_modified")){
+		attrname = (t_symbol *)object_method((t_object *)data, gensym("getname"));
+	}
+	return MAX_ERR_NONE;
+}
+*/
---
+++ externals/odot/o.change/o.change.maxhelp
@@ -0,0 +1,191 @@
+{
+	"patcher" : 	{
+		"fileversion" : 1,
+		"rect" : [ 317.0, 44.0, 601.0, 562.0 ],
+		"bglocked" : 0,
+		"defrect" : [ 317.0, 44.0, 601.0, 562.0 ],
+		"openrect" : [ 0.0, 0.0, 0.0, 0.0 ],
+		"openinpresentation" : 0,
+		"default_fontsize" : 12.0,
+		"default_fontface" : 0,
+		"default_fontname" : "Arial",
+		"gridonopen" : 0,
+		"gridsize" : [ 10.0, 10.0 ],
+		"gridsnaponopen" : 0,
+		"toolbarvisible" : 1,
+		"boxanimatetime" : 200,
+		"imprint" : 0,
+		"enablehscroll" : 1,
+		"enablevscroll" : 1,
+		"devicewidth" : 0.0,
+		"boxes" : [ 			{
+				"box" : 				{
+					"maxclass" : "comment",
+					"text" : "reordering the messages in a bundle constitutes a change",
+					"linecount" : 3,
+					"patching_rect" : [ 190.0, 350.0, 150.0, 48.0 ],
+					"id" : "obj-9",
+					"fontname" : "Arial",
+					"numinlets" : 1,
+					"frgb" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"numoutlets" : 0,
+					"fontsize" : 12.0,
+					"presentation_rect" : [ 190.0, 354.0, 0.0, 0.0 ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "comment",
+					"text" : "output a bundle only if the contents have changed",
+					"linecount" : 2,
+					"patching_rect" : [ 190.0, 300.0, 150.0, 34.0 ],
+					"id" : "obj-8",
+					"fontname" : "Arial",
+					"numinlets" : 1,
+					"frgb" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"numoutlets" : 0,
+					"fontsize" : 12.0
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "o.message",
+					"text" : "/bar 4 5 6\n/foo 1 2 3",
+					"linecount" : 2,
+					"patching_rect" : [ 50.0, 130.0, 63.0, 34.0 ],
+					"id" : "obj-3",
+					"fontname" : "Arial",
+					"textcolor" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"fontsize" : 12.0,
+					"outlettype" : [ "" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "o.message",
+					"text" : "/foo 1 2 3\n/bar 4 5 7",
+					"linecount" : 2,
+					"patching_rect" : [ 300.0, 130.0, 62.0, 34.0 ],
+					"id" : "obj-2",
+					"fontname" : "Arial",
+					"textcolor" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"fontsize" : 12.0,
+					"outlettype" : [ "" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "o.message",
+					"text" : "/foo 1 2 3",
+					"patching_rect" : [ 220.0, 130.0, 62.0, 20.0 ],
+					"id" : "obj-7",
+					"fontname" : "Arial",
+					"textcolor" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"fontsize" : 12.0,
+					"outlettype" : [ "" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "newobj",
+					"text" : "o.print",
+					"patching_rect" : [ 140.0, 230.0, 44.0, 20.0 ],
+					"id" : "obj-5",
+					"fontname" : "Arial",
+					"numinlets" : 1,
+					"numoutlets" : 0,
+					"fontsize" : 12.0
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "o.message",
+					"text" : "/foo 1 2 3\n/bar 4 5 6",
+					"linecount" : 2,
+					"patching_rect" : [ 140.0, 130.0, 62.0, 34.0 ],
+					"id" : "obj-4",
+					"fontname" : "Arial",
+					"textcolor" : [ 0.0, 0.0, 0.0, 1.0 ],
+					"numinlets" : 2,
+					"numoutlets" : 1,
+					"fontsize" : 12.0,
+					"outlettype" : [ "" ]
+				}
+
+			}
+, 			{
+				"box" : 				{
+					"maxclass" : "newobj",
+					"text" : "o.change",
+					"patching_rect" : [ 140.0, 180.0, 60.0, 20.0 ],
+					"id" : "obj-1",
+					"fontname" : "Arial",
+					"numinlets" : 1,
+					"numoutlets" : 1,
+					"fontsize" : 12.0,
+					"outlettype" : [ "FullPacket" ]
+				}
+
+			}
+ ],
+		"lines" : [ 			{
+				"patchline" : 				{
+					"source" : [ "obj-3", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-2", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-4", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-7", 0 ],
+					"destination" : [ "obj-1", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+, 			{
+				"patchline" : 				{
+					"source" : [ "obj-1", 0 ],
+					"destination" : [ "obj-5", 0 ],
+					"hidden" : 0,
+					"midpoints" : [  ]
+				}
+
+			}
+ ]
+	}
+
+}
