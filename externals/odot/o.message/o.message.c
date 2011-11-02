6f5c0f04d8612fffcc3ec3ec0f3211911a1367ba
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2011-11-02 20:36:45 +0000

    added strsep source to win version

--- externals/odot/o.message/o.message.c
+++ externals/odot/o.message/o.message.c
@@ -65,6 +65,39 @@
 #define OMESSAGE_MAX_MESSAGE_LENGTH 128
 #define BUFLEN 128
 
+#ifdef WIN_VERSION
+// currently we have to compile windows versions with gcc 3 on cygwin and i'm getting undefined
+// refs to strsep, so here it is fucker.
+char *
+strsep(stringp, delim)
+     register char **stringp;
+     register const char *delim;
+{
+	register char *s;
+	register const char *spanp;
+	register int c, sc;
+	char *tok;
+
+	if ((s = *stringp) == NULL)
+		return (NULL);
+	for (tok = s;;) {
+		c = *s++;
+		spanp = delim;
+		do {
+			if ((sc = *spanp++) == c) {
+				if (c == 0)
+					s = NULL;
+				else
+					s[-1] = 0;
+				*stringp = s;
+				return (tok);
+			}
+		} while (sc != 0);
+	}
+	/* NOTREACHED */
+}
+#endif
+
 
 enum {
 	OMESSAGE_U,
