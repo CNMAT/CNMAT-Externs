ca8cf56d2b5a6c36ce56187006b95f161fc0beae
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2012-06-29 05:28:40 +0000

    added max 6 sdk

---
+++ SDK/MaxSDK-6.0.4/html/navtree.js
@@ -0,0 +1,456 @@
+var NAVTREE =
+[
+  [ "Max API", "index.html", [
+    [ "Objects in C: A Roadmap", "index.html", null ],
+    [ "Related Pages", "pages.html", [
+      [ "Development System Information", "chapter_platform.html", null ],
+      [ "Anatomy of a Max Object", "chapter_anatomy.html", null ],
+      [ "Inlets and Outlets", "chapter_inout.html", null ],
+      [ "Atoms and Messages", "chapter_atoms.html", null ],
+      [ "The Scheduler", "chapter_scheduler.html", null ],
+      [ "Memory Allocation", "chapter_memory.html", null ],
+      [ "Anatomy of a MSP Object", "chapter_msp_anatomy.html", null ],
+      [ "Advanced Signal Object Topics", "chapter_msp_advanced.html", null ],
+      [ "Sending Messages, Calling Methods", "chapter_msgattached.html", null ],
+      [ "Anatomy of a UI Object", "chapter_ui_anatomy.html", null ],
+      [ "File Handling", "chapter_files.html", null ],
+      [ "Scripting the Patcher", "chapter_scripting.html", null ],
+      [ "Enhancements to Objects", "chapter_enhancements.html", null ],
+      [ "Data Structures", "chapter_datastructures.html", null ],
+      [ "Threading", "chapter_threading.html", null ],
+      [ "Drag'n'Drop", "chapter_dragndrop.html", null ],
+      [ "ITM", "chapter_itm.html", null ],
+      [ "Jitter Object Model", "chapter_jit_objectmodel.html", null ],
+      [ "Jitter Max Wrappers", "chapter_jit_maxwrappers.html", null ],
+      [ "Matrix Operator QuickStart", "chapter_jit_mopqs.html", null ],
+      [ "Matrix Operator Details", "chapter_jit_mopdetails.html", null ],
+      [ "OB3D QuickStart", "chapter_jit_ob3dqs.html", null ],
+      [ "OB3D Details", "chapter_jit_ob3ddetails.html", null ],
+      [ "Scheduler and Low Priority Queue Issues", "chapter_jit_sched.html", null ],
+      [ "Jitter Object Registration and Notification", "chapter_jit_notification.html", null ],
+      [ "Using Jitter Objects in C", "chapter_jit_usingobjs.html", null ],
+      [ "JXF File Specification", "chapter_jit_jxf.html", null ],
+      [ "Jitter Networking Specification", "chapter_jit_networking.html", null ],
+      [ "Appendix: Messages sent to Objects", "chapter_appendix_a.html", null ],
+      [ "Appendix: Providing Icons for UI Objects", "chapter_appendix_b.html", null ],
+      [ "Appendix: Additional Resources", "chapter_appendix_c.html", null ],
+      [ "Appendix: Updating Externals for Max 6", "chapter_appendix_d.html", null ]
+    ] ],
+    [ "Modules", "modules.html", [
+      [ "Attributes", "group__attr.html", null ],
+      [ "Classes", "group__class.html", [
+        [ "Old-Style Classes", "group__class__old.html", null ],
+        [ "Inlets and Outlets", "group__inout.html", null ]
+      ] ],
+      [ "Data Storage", "group__datastore.html", [
+        [ "Atom Array", "group__atomarray.html", null ],
+        [ "Database", "group__database.html", null ],
+        [ "Dictionary", "group__dictionary.html", null ],
+        [ "Hash Table", "group__hashtab.html", null ],
+        [ "Index Map", "group__indexmap.html", null ],
+        [ "Linked List", "group__linklist.html", null ],
+        [ "Quick Map", "group__quickmap.html", null ],
+        [ "String Object", "group__string.html", null ],
+        [ "Symbol Object", "group__symobject.html", null ],
+        [ "Dictionary Passing API", "group__dictobj.html", null ]
+      ] ],
+      [ "Data Types", "group__datatypes.html", [
+        [ "Atoms", "group__atom.html", null ],
+        [ "Atombufs", "group__atombuf.html", null ],
+        [ "Binbufs", "group__binbuf.html", null ],
+        [ "Symbols", "group__symbol.html", null ]
+      ] ],
+      [ "Files and Folders", "group__files.html", null ],
+      [ "Jitter", "group__jitter.html", [
+        [ "Atom Module", "group__atommod.html", null ],
+        [ "Attribute Module", "group__attrmod.html", null ],
+        [ "Binary Module", "group__binmod.html", null ],
+        [ "Class Module", "group__classmod.html", null ],
+        [ "Object Module", "group__objectmod.html", null ],
+        [ "Miscellaneous Utility Module", "group__utilitymod.html", null ],
+        [ "Linked List Module", "group__linklistmod.html", null ],
+        [ "Math Module", "group__mathmod.html", null ],
+        [ "Matrix Module", "group__matrixmod.html", null ],
+        [ "Max Wrapper Module", "group__maxwrapmod.html", null ],
+        [ "Memory Module", "group__memorymod.html", null ],
+        [ "MOP Module", "group__mopmod.html", null ],
+        [ "Parallel Utility Module", "group__parallelutilmod.html", null ],
+        [ "MOP Max Wrapper Module", "group__maxmopmod.html", null ],
+        [ "OB3D Module", "group__ob3dmod.html", null ],
+        [ "Operator Vector Module", "group__opvecmod.html", null ],
+        [ "QuickTime Codec Module", "group__qtcodecmod.html", null ],
+        [ "jit.qt.movie Module", "group__qtmoviemod.html", null ],
+        [ "jit.qt.record Module", "group__qtrecordmod.html", null ],
+        [ "QuickTime Utilties Module", "group__qtutilsmod.html", null ]
+      ] ],
+      [ "Memory Management", "group__memory.html", null ],
+      [ "Miscellaneous", "group__misc.html", [
+        [ "Console", "group__console.html", null ],
+        [ "Byte Ordering", "group__byteorder.html", null ],
+        [ "Extending expr", "group__expr.html", null ],
+        [ "Table Access", "group__tables.html", null ],
+        [ "Text Editor Windows", "group__texteditors.html", null ],
+        [ "Presets", "group__presets.html", null ],
+        [ "Event and File Serial Numbers", "group__evnum.html", null ],
+        [ "Loading Max Files", "group__loading__max__files.html", null ],
+        [ "Monitors and Displays", "group__jmonitor.html", null ],
+        [ "Windows", "group__jwind.html", null ],
+        [ "Mouse and Keyboard", "group__jmouse.html", null ]
+      ] ],
+      [ "MSP", "group__msp.html", [
+        [ "Buffers", "group__buffers.html", null ],
+        [ "PFFT", "group__pfft.html", null ],
+        [ "Poly", "group__poly.html", null ]
+      ] ],
+      [ "Objects", "group__obj.html", null ],
+      [ "Patcher", "group__patcher.html", [
+        [ "jpatcher", "group__jpatcher.html", null ],
+        [ "jbox", "group__jbox.html", null ],
+        [ "jpatchline", "group__jpatchline.html", null ],
+        [ "jpatcherview", "group__jpatcherview.html", null ]
+      ] ],
+      [ "Timing", "group__sched.html", [
+        [ "Clocks", "group__clocks.html", null ],
+        [ "Qelems", "group__qelems.html", null ],
+        [ "Systime API", "group__systime.html", null ],
+        [ "ITM Time Objects", "group__time.html", null ]
+      ] ],
+      [ "Threads", "group__threading.html", [
+        [ "Critical Regions", "group__critical.html", null ],
+        [ "Mutexes", "group__mutex.html", null ]
+      ] ],
+      [ "User Interface", "group__ui.html", [
+        [ "JGraphics", "group__jgraphics.html", [
+          [ "JSurface", "group__jsurface.html", null ],
+          [ "Scalable Vector Graphics", "group__jsvg.html", null ],
+          [ "JFont", "group__jfont.html", null ],
+          [ "JGraphics Matrix Transformations", "group__jmatrix.html", null ],
+          [ "JPattern", "group__jpattern.html", null ],
+          [ "Colors", "group__color.html", null ],
+          [ "TextField", "group__textfield.html", null ],
+          [ "TextLayout", "group__textlayout.html", null ],
+          [ "Popup Menus", "group__jpopupmenu.html", null ],
+          [ "Box Layer", "group__boxlayer.html", null ]
+        ] ],
+        [ "DataView", "group__jdataview.html", null ]
+      ] ],
+      [ "Unicode", "group__unicode.html", null ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Ex_ex", "structEx__ex.html", null ],
+      [ "t_atom", "structt__atom.html", null ],
+      [ "t_atomarray", "structt__atomarray.html", null ],
+      [ "t_atombuf", "structt__atombuf.html", null ],
+      [ "t_attr", "structt__attr.html", null ],
+      [ "t_buffer", "structt__buffer.html", null ],
+      [ "t_celldesc", "structt__celldesc.html", null ],
+      [ "t_charset_converter", "structt__charset__converter.html", null ],
+      [ "t_class", "structt__class.html", null ],
+      [ "t_datetime", "structt__datetime.html", null ],
+      [ "t_dictionary", "structt__dictionary.html", null ],
+      [ "t_dictionary_entry", "structt__dictionary__entry.html", null ],
+      [ "t_expr", "structt__expr.html", null ],
+      [ "t_fileinfo", "structt__fileinfo.html", null ],
+      [ "t_funbuff", "structt__funbuff.html", null ],
+      [ "t_hashtab", "structt__hashtab.html", null ],
+      [ "t_hashtab_entry", "structt__hashtab__entry.html", null ],
+      [ "t_indexmap", "structt__indexmap.html", null ],
+      [ "t_indexmap_entry", "structt__indexmap__entry.html", null ],
+      [ "t_jbox", "structt__jbox.html", null ],
+      [ "t_jboxdrawparams", "structt__jboxdrawparams.html", null ],
+      [ "t_jcolumn", "structt__jcolumn.html", null ],
+      [ "t_jdataview", "structt__jdataview.html", null ],
+      [ "t_jgraphics_font_extents", "structt__jgraphics__font__extents.html", null ],
+      [ "t_jit_attr", "structt__jit__attr.html", null ],
+      [ "t_jit_attr_filter_clip", "structt__jit__attr__filter__clip.html", null ],
+      [ "t_jit_attr_filter_proc", "structt__jit__attr__filter__proc.html", null ],
+      [ "t_jit_attr_offset", "structt__jit__attr__offset.html", null ],
+      [ "t_jit_attr_offset_array", "structt__jit__attr__offset__array.html", null ],
+      [ "t_jit_attribute", "structt__jit__attribute.html", null ],
+      [ "t_jit_gl_context_view", "structt__jit__gl__context__view.html", null ],
+      [ "t_jit_gl_drawinfo", "structt__jit__gl__drawinfo.html", null ],
+      [ "t_jit_glchunk", "structt__jit__glchunk.html", null ],
+      [ "t_jit_matrix_info", "structt__jit__matrix__info.html", null ],
+      [ "t_jit_mop", "structt__jit__mop.html", null ],
+      [ "t_jit_mop_io", "structt__jit__mop__io.html", null ],
+      [ "t_jit_op_info", "structt__jit__op__info.html", null ],
+      [ "t_jmatrix", "structt__jmatrix.html", null ],
+      [ "t_jrgb", "structt__jrgb.html", null ],
+      [ "t_jrgba", "structt__jrgba.html", null ],
+      [ "t_line_3d", "structt__line__3d.html", null ],
+      [ "t_linklist", "structt__linklist.html", null ],
+      [ "t_llelem", "structt__llelem.html", null ],
+      [ "t_matrix_conv_info", "structt__matrix__conv__info.html", null ],
+      [ "t_messlist", "structt__messlist.html", null ],
+      [ "t_object", "structt__object.html", null ],
+      [ "t_path", "structt__path.html", null ],
+      [ "t_pathlink", "structt__pathlink.html", null ],
+      [ "t_pfftpub", "structt__pfftpub.html", null ],
+      [ "t_privatesortrec", "structt__privatesortrec.html", null ],
+      [ "t_pt", "structt__pt.html", null ],
+      [ "t_pxdata", "structt__pxdata.html", null ],
+      [ "t_pxjbox", "structt__pxjbox.html", null ],
+      [ "t_pxobject", "structt__pxobject.html", null ],
+      [ "t_quickmap", "structt__quickmap.html", null ],
+      [ "t_rect", "structt__rect.html", null ],
+      [ "t_signal", "structt__signal.html", null ],
+      [ "t_size", "structt__size.html", null ],
+      [ "t_stack_splat", "structt__stack__splat.html", null ],
+      [ "t_string", "structt__string.html", null ],
+      [ "t_symbol", "structt__symbol.html", null ],
+      [ "t_symobject", "structt__symobject.html", null ],
+      [ "t_tinyobject", "structt__tinyobject.html", null ],
+      [ "t_wind_mouse_info", "structt__wind__mouse__info.html", null ],
+      [ "t_zll", "structt__zll.html", null ],
+      [ "word", "unionword.html", null ]
+    ] ],
+    [ "Data Structure Index", "classes.html", null ],
+    [ "Data Fields", "functions.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
