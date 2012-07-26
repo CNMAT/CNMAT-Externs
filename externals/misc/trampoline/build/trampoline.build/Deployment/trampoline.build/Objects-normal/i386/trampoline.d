e5db6e1345eed7fd56e583c904f7ff05c48f3e13
John MacCallum <John MacCallum@4f49de0d-27eb-0310-b575-da1ae09e33f1>
2012-07-26 00:04:37 +0000

    added trampoline

---
+++ externals/misc/trampoline/build/trampoline.build/Deployment/trampoline.build/Objects-normal/i386/trampoline.d
@@ -0,0 +1,26 @@
+dependencies: \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/trampoline.c \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/version.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_prefix.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_mess.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_types.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_maxtypes.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_byteorder.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_proto.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_sysmem.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_sysfile.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_systime.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_expr.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_path.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_qtimage.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_wind.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/version.c \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_obex.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/commonsyms.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_linklist.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_hashtab.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_atomarray.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_obex_util.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_dictionary.h \
+  /Users/john/Development/cnmat/trunk/max/externals/misc/trampoline/../../../c74support/max-includes/ext_obstring.h
---
+++ externals/misc/trampoline/version.h
@@ -0,0 +1,18 @@
+#ifndef NAME
+#define NAME "trampoline"
+#endif
+#ifndef DESCRIPTION
+#define DESCRIPTION "Trampoline tail call optimization for Max"
+#endif
+#ifndef AUTHORS
+#define AUTHORS "John MacCallum"
+#endif
+#ifndef COPYRIGHT_YEARS
+#define COPYRIGHT_YEARS "2011"
+#endif
+#ifndef SVN_REVISION
+#define SVN_REVISION "$LastChangedRevision: 587 $"
+#endif
+#ifndef VERSION
+#define VERSION "1.0"
+#endif
