8e9cb9e3b336ec8b2db1709220ea142f230bf7ee
maccallum <john@cnmat.berkeley.edu>
2012-08-29 17:23:32 -0700

    removed unnecessary files including old Makefile and Xcode project

--- src/harmonics~/harmonics~-classic.r
+++
@@ -1,9 +0,0 @@
-     resource 'vers' (1) {
-          0x01,               // Major release: digit before decimal
-          0x00,               // Minor release: digits after decimal
-          release,               // dev/alpha/beta/release
-          0x00,               // Build # (for pre-release)
-          0,                  // Region code (US)
-          "1.0 (Classic)",        // Major/minor release as short string
-          "1.0, © 1996,97,98,99,2000,01,02,03 CNMAT (Classic Version)"
-          };
