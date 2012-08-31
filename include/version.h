68296a9e4f270eb03ef083b268301c45666419ba
maccallum <john@cnmat.berkeley.edu>
2012-08-31 13:03:18 -0700

    added version.h to include

---
+++ include/version.h
@@ -0,0 +1,9 @@
+void version(void *x) {
+	post(NAME " Version " VERSION ", by " AUTHORS ".");
+	if (x != 0) {
+		// Not called from main()
+		post("Compiled " __TIME__ " " __DATE__);
+	}
+	post("Copyright (c) " COPYRIGHT_YEARS " Regents of the University of California.  All rights reserved.");
+}
+void version_post_copyright(void);
