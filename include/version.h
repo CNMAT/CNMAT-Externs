2bd287fddfbb80175223d05a9cc0477d27a8912b
maccallum <john.m@ccallum.com>
2012-09-02 00:44:59 -0700

    minor changes

--- include/version.h
+++ include/version.h
@@ -1,9 +1,10 @@
+#include "ext.h"
 void version(void *x) {
-	post(NAME " Version " VERSION ", by " AUTHORS ".");
+	//post(NAME " Version " VERSION ", by " AUTHORS ".");
 	if (x != 0) {
 		// Not called from main()
 		post("Compiled " __TIME__ " " __DATE__);
 	}
 	post("Copyright (c) " COPYRIGHT_YEARS " Regents of the University of California.  All rights reserved.");
 }
-void version_post_copyright(void);
+#define version_post_copyright()
