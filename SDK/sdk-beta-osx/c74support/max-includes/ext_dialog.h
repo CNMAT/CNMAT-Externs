e6820b97347f8a909e642dd19fc447358fb594c9
maccallum <john.m@ccallum.com>
2012-09-06 16:36:11 -0700

    removed max sdks--we now get them from github

--- SDK/sdk-beta-osx/c74support/max-includes/ext_dialog.h
+++
@@ -1,3 +0,0 @@
-void HiliteDefaultButton(DialogPtr dlg);
-void SetDlgTextItem(DialogPtr dlg, short item, Byte *text, short selectFlag);
-void GetDlgTextItem(DialogPtr dlg, short item, Byte *text);
--- SDK/sdk-beta-osx/c74support/max-includes/timeline-includes/myTypedefs.h
+++
@@ -1,8 +0,0 @@
-typedef	struct	windpub	Windpub;
-typedef struct	wind	Wind;
-typedef struct atombuf {
-	long a_argc;
-	struct atom a_argv[1];
-}Atombuf;
-typedef	struct	binbuf	Binbuf;
- 
