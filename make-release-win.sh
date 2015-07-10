b13b340a1672ae6562b6017a554b5945793ade78
Ilya Rostovtsev <ilyaforpresident@gmail.com>
2015-07-10 16:18:48 -0700

    shell script for authoring windows releases

---
+++ make-release-win.sh
@@ -0,0 +1,12 @@
+archive_name=CNMAT_Externals-Max-Win32-`git describe --tags --long`-`git branch | egrep '^\*' | awk '{print $2}'`.zip
+dirs=(help media misc)
+
+mkdir CNMAT-Externals && mkdir CNMAT-Externals/externals
+cp build/Release/*.mxe CNMAT-Externals/externals
+
+for f in ${dirs[*]}
+do
+	[ -e $f ] && cp -r $f CNMAT-Externals
+done
+
+zip -r $archive_name CNMAT-Externals && rm -r CNMAT-Externals
