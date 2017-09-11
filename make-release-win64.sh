ab9c605211842af5db9398702182454e05b4c84c
ramagottfried <rama.gottfried@gmail.com>
2017-09-11 12:25:35 +0200

    removed kludge from make release script, added win64 script

--- make-release-win64.sh
+++ make-release-win64.sh
@@ -1,15 +1,12 @@
-archive_name=CNMAT_Externals-Max-Win32-`git describe --tags --long`-`git branch | egrep '^\*' | awk '{print $2}'`.zip
+archive_name=CNMAT_Externals-Max-Win64-`git describe --tags --long`-`git branch | egrep '^\*' | awk '{print $2}'`.zip
 dirs=(help media misc)
 
 mkdir CNMAT-Externals && mkdir CNMAT-Externals/externals
-cp build/Release/*.mxe CNMAT-Externals/externals
+cp build/Release/*.mxe64 CNMAT-Externals/externals
 
 for f in ${dirs[*]}
 do
 	[ -e $f ] && cp -r $f CNMAT-Externals
 done
-
-# force-copy an old version of resonators~
-cp -f kludge/resonators~.mxe CNMAT-Externals/externals
 
 zip -r $archive_name CNMAT-Externals && rm -r CNMAT-Externals
