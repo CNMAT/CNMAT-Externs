f2e97ee3e83301c05f56b209c55f55ec5921ef8d
ramagottfried <rama.gottfried@gmail.com>
2017-10-06 15:48:09 +0200

    removed test code from bench.c, removed extra libgsl link from bessel build phases

--- src/bench/bench.c
+++ src/bench/bench.c
@@ -201,32 +201,6 @@ void bench_list(t_bench *x, t_symbol *msg, short argc, t_atom *argv){
 	}
 }
 
-void bench_bong(t_bench *x)
-{
-  LARGE_INTEGER StartingTime, EndingTime, ElapsedMicroseconds;
-  LARGE_INTEGER Frequency;
-
-  QueryPerformanceFrequency(&Frequency);
-  QueryPerformanceCounter(&StartingTime);
-
-  // Activity to be timed
-
-  QueryPerformanceCounter(&EndingTime);
-  ElapsedMicroseconds.QuadPart = EndingTime.QuadPart - StartingTime.QuadPart;
-
-
-  //
-  // We now have the elapsed number of ticks, along with the
-  // number of ticks-per-second. We use these values
-  // to convert to the number of elapsed microseconds.
-  // To guard against loss-of-precision, we convert
-  // to microseconds *before* dividing by ticks-per-second.
-  //
-
-  ElapsedMicroseconds.QuadPart *= 1000000;
-  ElapsedMicroseconds.QuadPart /= Frequency.QuadPart;
-}
-
 void bench_bang(t_bench *x){
 	if(x->t_objmode == BENCH_OUT){
 #ifdef _WIN32
