ea893112a9008a215b213a9ccc0ea3ac915d473d
Jeremy Wagner <wagne342@umn.edu>
2022-01-27 17:59:02 -0800

    Updates to Timewarp~
    • added support for Apple-Silicon
    • changed -std to g++=C11 to allow boost to compile under clang 13.0.0

---
+++ mspexternals/tempocurving/timewarp~/version.h
@@ -0,0 +1,18 @@
+#ifndef NAME
+#define NAME "tempo_editor"
+#endif
+#ifndef DESCRIPTION
+#define DESCRIPTION "GUI for the tempocurver"
+#endif
+#ifndef AUTHORS
+#define AUTHORS "John MacCallum "
+#endif
+#ifndef COPYRIGHT_YEARS
+#define COPYRIGHT_YEARS "2009 "
+#endif
+#ifndef SVN_REVISION
+#define SVN_REVISION "$LastChangedRevision: 587 $ "
+#endif
+#ifndef VERSION
+#define VERSION "1.6.7"
+#endif
